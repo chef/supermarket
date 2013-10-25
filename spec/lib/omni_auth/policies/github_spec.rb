3168d3bc17ad9390907d17edd91d5391c495a16f
Seth Vargo <sethvargo@gmail.com>
2013-10-25 14:37:24 -0400

    Move OmniAuth mocks into a support file

--- spec/lib/omni_auth/policies/github_spec.rb
+++ spec/lib/omni_auth/policies/github_spec.rb
@@ -1,34 +1,15 @@
 require 'spec_helper'
 
 describe OmniAuth::Policies::Github do
-  let(:auth) do
-    {
-      'provider' => 'github',
-      'uid' => '408570',
-      'info' => {
-        'nickname' => 'sethvargo',
-        'email' => 'sethvargo@gmail.com',
-        'name' => 'Seth Vargo',
-        'image' => 'https://2.gravatar.com/avatar/87f282c6c2cdad13100dffe8c1daf77d?d=https%3A%2F%2Fidenticons.github.com%2Fb75f58301305183b958bf0488a88add8.png&r=x',
-        'urls' => {
-          'GitHub' => 'https://github.com/sethvargo',
-          'Blog' => 'sethvargo.com'
-        }
-      },
-      'credentials' => {
-        'token' => 'aaa0a0aa0aaa000aaaa0aaa00aaa000a00000a00',
-        'expires' => false
-      }
-    }
-  end
+  let(:auth) { OmniAuth.config.mock_auth[:github] }
 
   subject { described_class.new(auth) }
 
-  its(:first_name) { should eq('Seth') }
-  its(:last_name) { should eq('Vargo') }
-  its(:email) { should eq('sethvargo@gmail.com') }
-  its(:username) { should eq('sethvargo') }
-  its(:image_url) { should eq('https://2.gravatar.com/avatar/87f282c6c2cdad13100dffe8c1daf77d?d=https%3A%2F%2Fidenticons.github.com%2Fb75f58301305183b958bf0488a88add8.png&r=x') }
-  its(:uid) { should eq('408570') }
-  its(:oauth_token) { should eq('aaa0a0aa0aaa000aaaa0aaa00aaa000a00000a00') }
+  its(:first_name)  { should eq('John') }
+  its(:last_name)   { should eq('Doe') }
+  its(:email)       { should eq('johndoe@example.com') }
+  its(:username)    { should eq('johndoe') }
+  its(:image_url)   { should eq('https://image-url.com') }
+  its(:uid)         { should eq('12345') }
+  its(:oauth_token) { should eq('oauth_token') }
 end
---
+++ spec/support/omni_auth.rb
@@ -0,0 +1,36 @@
+module OmniAuth
+  config.test_mode = true
+
+  # GitHub
+  config.mock_auth[:github] = OmniAuth::AuthHash.new({
+    provider: 'github',
+    uid: '12345',
+    info: {
+      nickname: 'johndoe',
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      image: 'https://image-url.com',
+    },
+    credentials: {
+      token: 'oauth_token',
+      expires: false
+    }
+  })
+
+  # Twitter
+  config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+    provider: 'twitter',
+    uid: '12345',
+    info: {
+      nickname: 'johndoe',
+      name: 'John Doe',
+      location: 'Pittsburgh, PA',
+      image: 'https://image-url.com',
+      description: 'This is a description',
+    },
+    credentials: {
+      token: 'oauth_token',
+      secret: 'oauth_secret'
+    }
+  })
+end
