eb2d06a420f6f8f50b367a61f8329123e510d479
Nathan L Smith <nlloyds@gmail.com>
2013-10-25 15:11:30 -0500

    add jquery/jquery_ujs; add vendor assets to js spec paths

--- spec/javascripts/config/sprockets.js
+++ spec/javascripts/config/sprockets.js
@@ -4,6 +4,7 @@ var SprocketsChain = require('sprockets-chain'),
 
 // Append paths to the list of load paths
 sc.appendPath('app/assets/javascripts');
+sc.appendPath('vendor/assets/javascripts');
 
 // Get ordered array of individual absolute file paths that compose the
 // `application.js` bundle
