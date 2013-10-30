34335794ad4ae6d5b601020a83e38eea5f996114
Nathan L Smith <nlloyds@gmail.com>
2013-10-30 17:31:42 -0500

    Styles for new ICLA form and more
    Also:
    
    * Adds polyfill for placeholder/autofocus support in IE 9
    * Moves jquery_ujs to jquery/rails because it looks nicer
    * Modify the form errors so they apply a class to the inputs instead of
      wrapping them in a div, which screws up the layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,6 @@
 //= require jquery-2.0.3
-//= require jquery_ujs-3.0.4
+//= require jquery/html5support-4ea69b
+//= require jquery/rails-3.0.4
+
+//= require application/polyfills
 //= require application/flash
---
+++ config/initializers/form_helper_errors.rb
@@ -0,0 +1,14 @@
+# Instead of wrapping form fields with errors in a div (which messes up the grid
+# layout), just add an error class to them.
+#
+# See http://stackoverflow.com/a/8380400/161787
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  class_attr_index = html_tag.index 'class="'
+
+  if class_attr_index
+    html_tag.insert class_attr_index+7, 'error '
+  else
+    html_tag.insert html_tag.index('>'), ' class="error"'
+  end
+end
