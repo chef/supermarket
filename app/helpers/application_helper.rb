8b8f3a894712ccdc4dbe6d2ddf9d856de62302d5
Seth Vargo <sethvargo@gmail.com>
2013-11-01 11:00:36 -0400

    Remove helper and use standard HTML

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,21 +7,4 @@ module ApplicationHelper
   def auth_path(provider)
     "/auth/#{provider}"
   end
-
-  #
-  # Create an OmniAuth button for the given provider.
-  #
-  # @example
-  #   omniauth_button('GitHub')
-  #
-  # @param [String] provider
-  #   the string of text you want the link to appear as
-  #
-  def omniauth_button(provider)
-    key = provider.downcase.to_sym
-
-    content_tag(:li, class: key) do
-      link_to(provider, auth_path(key))
-    end
-  end
 end
