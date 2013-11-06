d7ceec88c9eb4fc741bbdb11f14c09e57922cbd7
Nathan L Smith <nlloyds@gmail.com>
2013-11-05 23:17:18 -0600

    shorten parts in db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,4 @@ parts = %w[ head body ].inject({}) do |result, part|
   result
 end
 
-Icla.create_with({
-  :head => parts['head'],
-  :body => parts['body']
-}).find_or_create_by(:version => Supermarket::Config.icla_version)
+Icla.create_with(parts).find_or_create_by(:version => Supermarket::Config.icla_version)
