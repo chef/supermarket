7dc641716f95a5efbfed9518d409bea13c2f0789
Nathan L Smith <nlloyds@gmail.com>
2013-11-06 23:20:47 -0600

    remove comment from helper spec

--- spec/helpers/icla_signatures_helper_spec.rb
+++ spec/helpers/icla_signatures_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the IclaSignaturesHelper. For example:
-#
-# describe IclaSignaturesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe IclaSignaturesHelper do
   describe '#markdown' do
     it 'renders markdown' do
