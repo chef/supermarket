d06e84ed983d16bf807dd8af997490f6607ab408
Brian Cobb <bcobb@uwalumni.com>
2014-01-23 15:35:43 -0600

    Move sign_icla helper into feature helpers

--- spec/features/icla_signing_spec.rb
+++ spec/features/icla_signing_spec.rb
@@ -11,22 +11,4 @@ describe 'signing a ICLA' do
     expect(page).to have_no_content 'Sign ICLA'
   end
 
-  def sign_icla
-    click_link 'Sign ICLA'
-
-    fill_in 'icla_signature_first_name', with: 'John'
-    fill_in 'icla_signature_last_name', with: 'Doe'
-    fill_in 'icla_signature_company', with: 'Chef'
-    fill_in 'icla_signature_email', with: 'john@example.com'
-    fill_in 'icla_signature_phone', with: '(555) 555-5555'
-    fill_in 'icla_signature_address_line_1', with: '1 Chef Way'
-    fill_in 'icla_signature_city', with: 'Seattle'
-    fill_in 'icla_signature_state', with: 'WA'
-    fill_in 'icla_signature_zip', with: '12345'
-    fill_in 'icla_signature_country', with: 'USA'
-
-    find("label[for='icla_signature_agreement']").click
-
-    find_button('Sign ICLA').click
-  end
 end
