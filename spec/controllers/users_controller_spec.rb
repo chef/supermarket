7884a3b61f106e39b3e9a78c77f6f7bcabb416db
Brett Chalupa <brettchalupa@gmail.com>
2014-01-27 14:51:20 -0500

    Write spec for UsersController#show.

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe UsersController do
+  let(:user) { create(:user) }
+
+  describe 'GET #show' do
+    it 'assigns a user' do
+      get :show, id: user.id
+
+      expect(assigns[:user]).to eql(user)
+    end
+  end
+end
