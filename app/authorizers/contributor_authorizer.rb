1d70939aa7dff40c148574ca95ee13b28216af41
Brett Chalupa <brettchalupa@gmail.com>
2014-01-28 17:11:03 -0500

    Document ClaSignature and Contributor Authorizers.

--- app/authorizers/contributor_authorizer.rb
+++ app/authorizers/contributor_authorizer.rb
@@ -3,6 +3,15 @@ require 'authorizer/base'
 class ContributorAuthorizer < Authorizer::Base
   alias contributor record
 
+  #
+  # A user who is an admin of the contributor's organization while the
+  # contributor is not an admin can delete a +Contributor+.
+  #
+  # A user who is an admin of the contributor's organization while the contributor
+  # is not the last admin can can delete a +Contributor+.
+  #
+  # @return [Boolean]
+  #
   def destroy?
     organization = contributor.organization
 
@@ -15,6 +24,11 @@ class ContributorAuthorizer < Authorizer::Base
     end
   end
 
+  #
+  # A user who can destroy a +Contributor+ can update a +Contributor+.
+  #
+  # @return [Boolean]
+  #
   def update?
     destroy?
   end
