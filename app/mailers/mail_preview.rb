bc39fa5d44926170066efda6067d1831c953953e
Tristan O'Neil <tristanoneil@gmail.com>
2014-01-29 10:39:41 -0500

    Minor Fixes
    This should resolve an issue that was causing CLA notification
    emails to have relative links to CLAs. This also ensures
    that test and development dependencies aren't installed in
    production.

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -12,7 +12,12 @@ if Rails.env.development?
       user = User.where(first_name: 'John', last_name: 'Doe',
         email: 'john@example.com').first_or_create!(password: 'password',
         password_confirmation: 'password')
-      cla_signature = user.icla_signatures.create
+
+      cla_signature = user.icla_signatures.where(first_name: 'John',
+        last_name: 'Doe', email: 'john@example.com', phone: '888-555-5555',
+        address_line_1: '123 Fake Street', city: 'Burlington',
+        state: 'Vermont', zip: '05401', country: 'United States')
+        .first_or_create!(agreement: '1')
 
       mail = ClaSignatureMailer.notification_email(cla_signature)
     end
