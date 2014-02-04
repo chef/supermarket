f15b560503584aab837693828a640dc8b185777f
Tristan O'Neil <tristanoneil@gmail.com>
2014-02-04 11:15:26 -0500

    Only configure Sentry if configuration exists
    This prevents Rails from failing on startup if a Sentry URL
    hasn't been configured.

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,7 @@
-require 'raven'
+if Supermarket::Config.sentry_url.present?
+  require 'raven'
 
-Raven.configure do |config|
-  config.dsn = Supermarket::Config.sentry_url
+  Raven.configure do |config|
+    config.dsn = Supermarket::Config.sentry_url
+  end
 end
