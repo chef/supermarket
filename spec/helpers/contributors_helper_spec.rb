9e20f404342c9ad2288c9774976661f4be1b9a00
Tristan O'Neil <tristanoneil@gmail.com>
2014-02-19 12:25:25 -0500

    Make managing CCLAs and contributors more cohesive
    This adds links between viewing a CCLA and managing contributors
    to make the UI more cohesive. It also moves some view logic for
    remove contributors links into a helper method and tests it.

---
+++ spec/helpers/contributors_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe ContributorsHelper do
+  let(:user) { create(:user) }
+
+  describe '#remove_contributor_link_for' do
+    it 'returns a remove myself contributor link when the current user owns the contributor' do
+      helper.stub(:current_user) { user }
+      contributor = create(:contributor, user: user)
+
+      expect(helper.remove_contributor_link_for(contributor)).to match(/<a.*remove_self/)
+    end
+
+    it 'returns a remove contributor link  when the current user owns the contributor' do
+      helper.stub(:current_user) { user }
+      contributor = create(:contributor)
+
+      expect(helper.remove_contributor_link_for(contributor)).to match(/<a.*remove_contributor/)
+    end
+  end
+end
