21d90076989115c7e2a77bf8ffe8b2d5256f64ea
Brett Chalupa <brettchalupa@gmail.com>
2014-02-19 11:08:18 -0500

    Curry workers return nil for invalid repositories
    Specifically, the Curry::ClaValidationWorker and
    Curry::ImportUnknownPullRequestCommitAuthorsWorker return nil when an
    ActiveRecord::RecordNotFound error is raised. This prevents the worker chain
    from continuing for pull requests that may no longer exists.

--- spec/workers/curry/cla_validation_worker_spec.rb
+++ spec/workers/curry/cla_validation_worker_spec.rb
@@ -2,15 +2,33 @@ require 'spec_helper'
 
 describe Curry::ClaValidationWorker do
 
-  context 'when processing a Pull Request Update' do
+  context 'when processing a pull pequest' do
 
-    it "annotates the Update's associated Pull Request" do
+    it "annotates the pull request when the PR exists" do
       repository = create(:repository)
       pull_request = repository.pull_requests.create!(number: '1')
 
       expect_any_instance_of(Curry::PullRequestAnnotator).to receive(:annotate)
 
       worker = Curry::ClaValidationWorker.new
+      worker.perform(pull_request.id)
+    end
+
+    it "does not annotate the pull request if the pull request no longer exists" do
+      expect_any_instance_of(Curry::PullRequestAnnotator).
+        to_not receive(:annotate)
+
+      worker = Curry::ClaValidationWorker.new
+      worker.perform(nil)
+    end
+
+    it "does not annotate the pull request if the pull request's repository no longer exists" do
+      expect_any_instance_of(Curry::PullRequestAnnotator).
+        to_not receive(:annotate)
+
+      worker = Curry::ClaValidationWorker.new
+      pull_request = create(:pull_request)
+      pull_request.repository.delete
       worker.perform(pull_request.id)
     end
 
--- spec/workers/curry/import_unknown_pull_request_commit_authors_worker_spec.rb
+++ spec/workers/curry/import_unknown_pull_request_commit_authors_worker_spec.rb
@@ -27,4 +27,20 @@ describe Curry::ImportUnknownPullRequestCommitAuthorsWorker do
     worker.perform(pull_request.id)
   end
 
+  it 'does not run the CLA Validation Worker if the pull request does not exist' do
+    expect(Curry::ClaValidationWorker).to_not receive(:peform_async)
+
+    worker = Curry::ImportUnknownPullRequestCommitAuthorsWorker.new
+    worker.perform(nil)
+  end
+
+  it "does not run the CLA Validation Worker if the pull request's repository no longer exists" do
+    expect(Curry::ClaValidationWorker).to_not receive(:peform_async)
+
+    worker = Curry::ImportUnknownPullRequestCommitAuthorsWorker.new
+    pull_request = create(:pull_request)
+    pull_request.repository.delete
+    worker.perform(pull_request.id)
+  end
+
 end
