1c0fd386ebade3d0119bee7aff67b48048d2afa2
Brian and Brett <brian+bcobb+brettchalupa@cramerdev.com>
2014-03-19 11:36:43 -0400

    Update the cookbook show view to be more semantic.
    This implements a joke of a method CookbookVersion#ghfmd_readme that should be replaced
    with the actual readme content of a cookbook version that gets parsed with
    GitHub Flavoured Markdown.

--- app/models/cookbook_version.rb
+++ app/models/cookbook_version.rb
@@ -33,4 +33,13 @@ class CookbookVersion < ActiveRecord::Base
   def to_param
     version.gsub(/\./, '_')
   end
+
+  #
+  # returns the README parsed with GitHub flavoured markdown
+  #
+  # TODO: implement this
+  #
+  def ghfmd_readme
+    "hi, I am a great readme"
+  end
 end
