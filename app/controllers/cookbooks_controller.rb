7101fb871519ff627bc1fbe04924697855c5c4b0
Brian and Brett <brian+bcobb+brettchalupa@cramerdev.com>
2014-03-20 11:13:02 -0400

    View structure for collaborators and platforms

--- app/controllers/cookbooks_controller.rb
+++ app/controllers/cookbooks_controller.rb
@@ -2,11 +2,18 @@ class CookbooksController < ApplicationController
   #
   # GET /cookbooks/:id
   #
+  # @todo Provide real maintainer and collaborator records to the view once
+  # oc-id is in place
+  #
   # Displays a cookbook.
   #
   def show
     @cookbook = Cookbook.with_name(params[:id]).first!
     @latest_version = @cookbook.get_version!('latest')
+    @cookbook_versions = @cookbook.cookbook_versions
+    @maintainer = User.first
+    @collaborators = [User.first]
+    @platforms = []
   end
 
   #
--- spec/features/cookbook_view_spec.rb
+++ spec/features/cookbook_view_spec.rb
@@ -2,7 +2,8 @@ require 'spec_feature_helper'
 
 describe 'viewing a cookbook' do
   it 'displays cookbook details if the cookbook exists' do
-    cookbook = create(:cookbook)
+    maintainer = create(:user)
+    cookbook = create(:cookbook) # TODO: give this cookbook a real maintainer
 
     visit cookbook_path(cookbook)
 
