e79da42282ff35cee293d6b4c9d9cca525d5656d
Josh Black <raskchanky@gmail.com>
2014-03-25 10:43:28 -0700

    Add show action. Refactor cookbook and version into before_filter

--- app/controllers/cookbook_versions_controller.rb
+++ app/controllers/cookbook_versions_controller.rb
@@ -1,21 +1,36 @@
 class CookbookVersionsController < ApplicationController
+  before_filter :set_cookbook_and_version
+
   #
-  # GET /cookbooks/:cookbook_id/versions/:version
+  # GET /cookbooks/:cookbook_id/versions/:version/download
   #
   # Redirects the user to the cookbook artifact
   #
   def download
-    cookbook = Cookbook.with_name(params[:cookbook_id]).first!
-    version = cookbook.get_version!(params[:version])
-    CookbookVersion.increment_counter(:download_count, version.id)
-    Cookbook.increment_counter(:download_count, cookbook.id)
+    CookbookVersion.increment_counter(:download_count, @version.id)
+    Cookbook.increment_counter(:download_count, @cookbook.id)
 
     SegmentIO.track_server_event(
       'cookbook_version_downloaded',
-      cookbook: cookbook.name,
-      version: version.version
+      cookbook: @cookbook.name,
+      version: @version.version
     )
 
-    redirect_to version.tarball.url
+    redirect_to @version.tarball.url
+  end
+
+  #
+  # GET /cookbooks/:cookbook_id/versions/:version
+  #
+  # Displays information about this particular cookbook version
+  #
+  def show
+  end
+
+  private
+
+  def set_cookbook_and_version
+    @cookbook = Cookbook.with_name(params[:cookbook_id]).first!
+    @version = @cookbook.get_version!(params[:version])
   end
 end
