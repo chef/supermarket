aa2a839f48bf6fe63b6339e4fc953bec02450ac6
Brian Cobb <bcobb@uwalumni.com>
2014-04-01 10:53:26 -0500

    Exercise cookbook views in a less direct way

--- spec/features/cookbook_following_spec.rb
+++ spec/features/cookbook_following_spec.rb
@@ -1,29 +1,122 @@
 require 'spec_feature_helper'
 
 describe 'cookbook following' do
-  before do
-    sign_in(create(:user))
-    maintainer = create(:user)
-    cookbook = create(:cookbook) # TODO: give this cookbook a real maintainer
+  shared_examples "a page which has a Follow button" do
+    it 'allows a user to follow a cookbook' do
+      follow_relation 'follow'
 
-    visit '/'
-    follow_relation 'cookbooks'
+      expect(page).to have_xpath("//a[starts-with(@rel, 'unfollow')]")
+    end
 
-    within '.recently-updated' do
-      follow_relation 'cookbook'
+    it 'allows a user to unfollow a cookbook' do
+      follow_relation 'follow'
+      follow_relation 'unfollow'
+
+      expect(page).to have_xpath("//a[starts-with(@rel, 'follow')]")
     end
   end
 
-  it 'allows a user to follow a cookbook' do
-    follow_relation 'follow'
+  shared_examples 'a page which can manage cookbook URLs' do
+    it 'displays success message when saved' do
+      within '.cookbook-details' do
+        follow_relation 'edit-cookbook-urls'
+        fill_in 'Source URL', with: 'http://example.com/source'
+        fill_in 'Issues URL', with: 'http://example.com/tissues'
+        submit_form
+      end
 
-    expect(page).to have_xpath("//a[starts-with(@rel, 'unfollow')]")
+      expect_to_see_success_message
+    end
+
+    it 'displays a failure message when invalid urls are entered' do
+      within '.cookbook-details' do
+        follow_relation 'edit-cookbook-urls'
+        fill_in 'Source URL', with: 'example'
+        fill_in 'Issues URL', with: 'example'
+
+        expect(page).to have_selector('.error')
+      end
+    end
   end
 
-  it 'allows a user to unfollow a cookbook' do
-    follow_relation 'follow'
-    follow_relation 'unfollow'
+  context 'when navigating from the cookbooks directory' do
+    before do
+      sign_in(create(:user))
+      create(:cookbook) # TODO: give this cookbook a real maintainer
 
-    expect(page).to have_xpath("//a[starts-with(@rel, 'follow')]")
+      visit '/'
+      follow_relation 'cookbooks'
+
+      within '.recently-updated' do
+        follow_relation 'cookbook'
+      end
+    end
+
+    it_behaves_like 'a page which has a Follow button'
+    it_behaves_like 'a page which can manage cookbook URLs'
+  end
+
+  context 'when navigating from the feed of recently updated cookbooks' do
+    before do
+      sign_in(create(:user))
+      create(:cookbook) # TODO: give this cookbook a real maintainer
+
+      visit '/'
+      click_link 'Cookbooks'
+
+      within '.recently-updated' do
+        click_link 'View All'
+      end
+
+      within '.cookbook_listing' do
+        relations('cookbook').first.click
+      end
+    end
+
+    it_behaves_like 'a page which has a Follow button'
+    it_behaves_like 'a page which can manage cookbook URLs'
+  end
+
+  context 'when navigating from a Category' do
+    before do
+      sign_in(create(:user))
+      cookbook = create(:cookbook) # TODO: give this cookbook a real maintainer
+
+      visit '/'
+      click_link 'Cookbooks'
+
+      within '.categories' do
+        click_link cookbook.category.name
+      end
+
+      within '.cookbook_listing' do
+        relations('cookbook').first.click
+      end
+    end
+
+    it_behaves_like 'a page which has a Follow button'
+    it_behaves_like 'a page which can manage cookbook URLs'
+  end
+
+  context 'when navigating from search results', focus: true do
+    before do
+      sign_in(create(:user))
+      create(:cookbook, name: 'AmazingCookbook')
+
+      visit '/'
+      click_link 'Cookbooks'
+
+      within '.page' do
+        fill_in 'q', with: 'Amazing'
+        submit_form
+      end
+
+      within '.cookbook_listing' do
+        relations('cookbook').first.click
+      end
+    end
+
+    it_behaves_like 'a page which has a Follow button'
+    it_behaves_like 'a page which can manage cookbook URLs'
   end
 end
