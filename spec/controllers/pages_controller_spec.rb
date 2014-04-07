b736fef550b26d3a3fb3f1aa9d0e5cce962eb1b5
Brett Chalupa <brett@brettchalupa.com>
2014-04-07 16:14:43 -0400

    Create the dashboard & welcome views.
    The dashboard is what an authenticated user sees. It displays the activity feed
    for the cookbooks they follow and easy to find links for the cookbooks they own
    and contribute to.
    
    The welcome page is what a non-authenticated user sees. It introduces them to
    what Supermarket is and highlights what they can do on Supermarket.
    
    The content is not all fleshed out yet, but it will soon include things like
    featured cookbooks, cookbook activity feeds and the cookbooks that a user owns
    and contributes to.

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe PagesController do
+  let(:user) { create(:user) }
+
+  describe 'GET #welcome' do
+    context 'user is not signed in' do
+      it 'responds with a 200' do
+        get :welcome
+
+        expect(response.status.to_i).to eql(200)
+      end
+
+      it 'sends recently updated cookbook count to the view' do
+        get :welcome
+
+        expect(assigns[:recently_updated_count]).to_not be_nil
+      end
+
+      it 'sends cookbook count to the view' do
+        get :welcome
+
+        expect(assigns[:cookbook_count]).to_not be_nil
+      end
+
+      it 'sends download count to the view' do
+        get :welcome
+
+        expect(assigns[:download_count]).to_not be_nil
+      end
+
+      it 'sends user count to the view' do
+        get :welcome
+
+        expect(assigns[:user_count]).to_not be_nil
+      end
+    end
+
+    context 'user is signed in' do
+      before { sign_in user }
+
+      it 'redirects the user to the dashboard' do
+        get :welcome
+
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+  end
+
+  describe 'GET #dashboard' do
+    context 'user is signed in' do
+      before { sign_in user }
+
+      # xit until cookbook ownership and contributors is in place
+      xit 'assigns cookbooks' do
+        get :dashboard
+
+        expect(assigns[:cookbooks]).to_not be_nil
+      end
+    end
+
+    context 'user is not signed in' do
+      it 'redirects to the welcome page' do
+        get :dashboard
+
+        expect(response).to redirect_to(sign_in_path)
+      end
+    end
+  end
+end
