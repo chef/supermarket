533279547ade928a4f6fab4b09cc3d6aabec6df9
Josh Black <raskchanky@gmail.com>
2014-04-10 15:50:06 -0700

    add select2 for picking collaborators

--- app/assets/javascripts/collaborators.js
+++ app/assets/javascripts/collaborators.js
@@ -1,2 +1,25 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+  $('#cookbook_collaborator_user_id').select2({
+    placeholder: 'Search for a collaborator',
+    minimumInputLength: 3,
+    multiple: true,
+    width: '400px',
+    ajax: {
+      url: '/collaborators.json',
+      dataType: 'json',
+      quietMillis: 200,
+      data: function (term, page) {
+        return { q: term };
+      },
+      results: function (data, page) {
+        return {results: data.collaborators};
+      }
+    },
+    formatSelection: function(collaborator, container) {
+      return collaborator.email;
+    },
+    formatResult: function(collaborator, container) {
+      return collaborator.first_name + ' ' + collaborator.last_name + '(' + collaborator.email + ')';
+    }
+  });
+});
