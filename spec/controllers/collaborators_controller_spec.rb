98cb38f8af60c1e97716e99c026b9b6d17ec14d3
Josh Black <raskchanky@gmail.com>
2014-04-10 15:53:33 -0700

    fixing specs

--- spec/controllers/collaborators_controller_spec.rb
+++ spec/controllers/collaborators_controller_spec.rb
@@ -2,49 +2,52 @@ require 'spec_helper'
 
 describe CollaboratorsController do
   let!(:fanny) { create(:user) }
+  let!(:hank) { create(:user, first_name: 'hank') }
+  let!(:hanky) { create(:user, first_name: 'hanky') }
   let!(:cookbook) { create(:cookbook, owner: fanny) }
 
+  before do
+    create(:icla_signature, user: fanny)
+    create(:icla_signature, user: hank)
+  end
+
   describe 'GET #index' do
-    it 'returns all collaborators by default' do
+    it 'returns all collaborators that have an icla by default' do
       get :index, format: :json
       c = assigns[:collaborators]
-      expect(c.size).to eql(1)
-      expect(c.first).to eql(fanny)
+      expect(c.size).to eql(2)
+      expect(c).to include(fanny)
+      expect(c).to include(hank)
       expect(response).to be_success
     end
 
-    it 'returns only collaborators matching the query string' do
-      jimmy = create(:user, first_name: 'Jimmy', last_name: 'Hoffa')
-
-      get :index, q: 'Jimmy', format: :json
+    it 'returns only collaborators matching the query string that have an icla' do
+      get :index, q: 'hank', format: :json
       c = assigns[:collaborators]
       expect(c.size).to eql(1)
-      expect(c.first).to eql(jimmy)
+      expect(c.first).to eql(hank)
       expect(response).to be_success
     end
   end
 
   describe 'destructive updates' do
-    let!(:sally) { create(:user) }
-    let!(:hank) { create(:user) }
-
     describe 'POST #create' do
       it 'creates a collaborator if the signed in user is the cookbook owner' do
         sign_in fanny
 
         expect do
-          post :create, cookbook_id: cookbook.to_param, user_id: hank.to_param, format: :json
+          post :create, cookbook_id: cookbook.to_param, cookbook_collaborator:{user_id: hank.to_param}
         end.to change { CookbookCollaborator.count }.by(1)
-        expect(response).to be_success
+        expect(response).to redirect_to(cookbook_path(cookbook))
       end
 
       it 'fails if the signed in user is not the cookbook owner' do
-        sign_in sally
+        sign_in hanky
 
         expect do
-          post :create, cookbook_id: cookbook.to_param, user_id: hank.to_param, format: :json
+          post :create, cookbook_id: cookbook.to_param, cookbook_collaborator:{user_id: hank.to_param}
         end.to_not change { CookbookCollaborator.count }
-        expect(response).to_not be_success
+        expect(response).to redirect_to(cookbook_path(cookbook))
       end
     end
 
@@ -57,7 +60,7 @@ describe CollaboratorsController do
         sign_in fanny
 
         expect do
-          delete :destroy, cookbook_id: cookbook.to_param, user_id: hank.to_param, format: :json
+          delete :destroy, cookbook_id: cookbook.to_param, id: hank.to_param, format: :json
         end.to change { CookbookCollaborator.count }.by(-1)
         expect(response).to be_success
       end
@@ -66,16 +69,16 @@ describe CollaboratorsController do
         sign_in hank
 
         expect do
-          delete :destroy, cookbook_id: cookbook.to_param, user_id: hank.to_param, format: :json
+          delete :destroy, cookbook_id: cookbook.to_param, id: hank.to_param, format: :json
         end.to change { CookbookCollaborator.count }.by(-1)
         expect(response).to be_success
       end
 
       it 'fails if the signed in user is not the cookbook owner and also not a collaborator' do
-        sign_in sally
+        sign_in hanky
 
         expect do
-          delete :destroy, cookbook_id: cookbook.to_param, user_id: hank.to_param, format: :json
+          delete :destroy, cookbook_id: cookbook.to_param, id: hank.to_param, format: :json
         end.to_not change { CookbookCollaborator.count }
         expect(response).to_not be_success
       end
@@ -84,7 +87,7 @@ describe CollaboratorsController do
         sign_in fanny
 
         expect do
-          delete :destroy, cookbook_id: cookbook.to_param, user_id: sally.to_param, format: :json
+          delete :destroy, cookbook_id: cookbook.to_param, id: hanky.to_param, format: :json
         end.to_not change { CookbookCollaborator.count }
         expect(response).to_not be_success
       end
@@ -94,7 +97,7 @@ describe CollaboratorsController do
         sign_in fanny
 
         expect do
-          delete :destroy, cookbook_id: redis.to_param, user_id: hank.to_param, format: :json
+          delete :destroy, cookbook_id: redis.to_param, id: hank.to_param, format: :json
         end.to_not change { CookbookCollaborator.count }
         expect(response).to_not be_success
       end
--- spec/models/cookbook_collaborator_spec.rb
+++ spec/models/cookbook_collaborator_spec.rb
@@ -8,12 +8,33 @@ describe CookbookCollaborator do
 
   context 'validations' do
     it { should validate_presence_of(:cookbook) }
-    it { should validate_presence_of(:user) }
+
+    it 'should require a user with an ICLA' do
+      c = create(:cookbook)
+      u = create(:user)
+      v = create(:user)
+      create(:icla_signature, user: v)
+
+      cc = CookbookCollaborator.new cookbook: c
+      expect(cc).to_not be_valid
+      expect(cc.errors[:user]).to_not be_empty
+      expect(cc.errors[:user].join).to match(/can't be blank/)
+
+      cc.user = u
+      expect(cc).to_not be_valid
+      expect(cc.errors[:user]).to_not be_empty
+      expect(cc.errors[:user].join).to match(/ICLA/)
+
+      cc.user = v
+      expect(cc).to be_valid
+      expect(cc.errors[:user]).to be_empty
+    end
   end
 
   it 'finds itself based on a cookbook and user' do
     u = create(:user)
     c = create(:cookbook)
+    create(:icla_signature, user: u)
     cc = CookbookCollaborator.create! cookbook: c, user: u
     expect(CookbookCollaborator.with_cookbook_and_user(c, u)).to eql(cc)
   end
