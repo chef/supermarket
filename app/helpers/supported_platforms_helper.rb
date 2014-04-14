fec511434cd6d7d9541140955e23ae2e450cdf51
Tristan O'Neil <tristanoneil@gmail.com>
2014-04-14 14:30:23 -0400

    Use hardcoded platform/icon key
    Since we're using an icon font for platform icons we need to
    return the corresponding character for a given platform.

--- app/helpers/supported_platforms_helper.rb
+++ app/helpers/supported_platforms_helper.rb
@@ -6,9 +6,26 @@ module SupportedPlatformsHelper
   #   platform = SupportedPlatform.new(name: 'ubuntu')
   #   supported_platform_icon(platform) == 'U'
   #
+  # TODO: Add fallback icon key, currently it just falls back to empty string.
+  #
   # @return [String] the icon
   #
   def supported_platform_icon(platform)
-    platform.name[0].upcase
+    {
+      'aws' => 'A',
+      'centos' => 'B',
+      'debian' => 'C',
+      'fedora' => 'D',
+      'freebsd' => 'E',
+      'linux_mint' => 'F',
+      'mac_osx' => 'G',
+      'oracle' => 'H',
+      'red_hat' => 'I',
+      'scientific' => 'J',
+      'smartos' => 'K',
+      'suse' => 'L',
+      'ubuntu' => 'M',
+      'windows' => 'N'
+    }.fetch(platform.name.parameterize('_'), '')
   end
 end
