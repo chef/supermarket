c8113981cf29f3a9386929f9c27739dfe76e013c
Brian Cobb <bcobb@uwalumni.com>
2014-04-14 10:38:28 -0500

    Filter PubSubHubbub secrets in VCR cassettes

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -1,3 +1,4 @@
+require 'cgi'
 require 'vcr'
 
 VCR.configure do |c|
@@ -6,6 +7,18 @@ VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = true
   c.filter_sensitive_data('<TOKEN>') do
     Supermarket::Config.github.fetch('access_token')
+  end
+  c.filter_sensitive_data('<PUBSUBHUBBUB_SECRET>') do
+    CGI.escape(Supermarket::Config.pubsubhubbub.fetch('hub_secret'))
+  end
+  c.filter_sensitive_data('<PUBSUBHUBBUB_SECRET>') do
+    Supermarket::Config.pubsubhubbub.fetch('hub_secret')
+  end
+  c.filter_sensitive_data('<PUBSUBHUBBUB_CALLBACK>') do
+    CGI.escape(Supermarket::Config.pubsubhubbub.fetch('callback_url'))
+  end
+  c.filter_sensitive_data('<PUBSUBHUBBUB_CALLBACK>') do
+    Supermarket::Config.pubsubhubbub.fetch('callback_url')
   end
   c.default_cassette_options = {
     match_requests_on: [:method, :uri, :body]
