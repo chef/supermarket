4c8ddf19ab868c15fb53932081bfc43a7364f020
Brett Chalupa <brett@brettchalupa.com>
2014-04-16 12:30:18 -0400

    Sleep for 1 sec for the cookbook_manage_spec.
    This gets the spec passing consistently (or so it seems). I believe the cause of
    this is the animation on the page, where Capybara is trying to click the submit
    form but sometimes the animation does not finish before it is done.

--- spec/features/cookbook_manage_spec.rb
+++ spec/features/cookbook_manage_spec.rb
@@ -13,6 +13,7 @@ describe "updating a cookbook's issues and source urls" do
       follow_relation 'edit-cookbook-urls'
       fill_in 'cookbook_source_url', with: 'http://example.com/source'
       fill_in 'cookbook_issues_url', with: 'http://example.com/tissues'
+      sleep 1
       submit_form
     end
 
@@ -20,7 +21,7 @@ describe "updating a cookbook's issues and source urls" do
     expect(find('.issues-url')[:href]).to eql('http://example.com/tissues')
   end
 
-  xit 'displays an error message when invalid urls are entered' do
+  it 'displays an error message when invalid urls are entered' do
     maintainer = create(:user)
     cookbook = create(:cookbook) # TODO: give this cookbook a real maintainer
 
