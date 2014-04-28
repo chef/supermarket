37b404c4251eecb26ab6d5bb4c7803b9b9b3a22b
Josh Black <raskchanky@gmail.com>
2014-04-28 15:09:31 -0700

    add name to collaborator

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,8 +5,10 @@ module UsersHelper
   # displayed. The default size is 48 pixels.
   #
   # @param user [User] the User for get the Gravatar for
-  # @param options [Hash] options for the Gravatar
+  # @param options [Hash] options for the user and Gravatar
   # @option options [Integer] :size (48) the size of the Gravatar in pixels
+  # @option options [Boolean] :show_name whether or not to display the user's
+  # name next to their gravatar
   #
   # @example Gravtar for current_user
   #   gravatar_for current_user, size: 72
@@ -23,6 +25,14 @@ module UsersHelper
     size = options[:size]
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    result = image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+
+    if options[:show_name]
+      content_tag(:span) do
+        result + " #{user.name}"
+      end
+    else
+      result
+    end
   end
 end
