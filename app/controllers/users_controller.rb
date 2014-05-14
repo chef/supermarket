5ed1b829a28ac8c03e7fd1576e403111c4c2eb49
Tristan O'Neil <tristanoneil@gmail.com>
2014-05-14 16:51:45 -0400

    Build out new user profile view
    This implements the new user profile view which now shows
    the users cookbook, the users collaborated cookbooks and cookbooks the
    user follows.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,21 @@ class UsersController < ApplicationController
   #
   # GET /users/:id
   #
-  # Display a user.
+  # Display a user and a users cookbooks for a given context. The cookbooks
+  # context is given via the tab paramter. Contexts include cookbooks the user
+  # collaborates on, cookbooks the user follows and the default context of cookbooks
+  # the user owns.
   #
   def show
     @user = Account.for('chef_oauth2').with_username(params[:id]).first!.user
+
+    case params[:tab]
+    when 'collaborates'
+      @cookbooks = @user.collaborated_cookbooks
+    when 'follows'
+      @cookbooks = @user.followed_cookbooks
+    else
+      @cookbooks = @user.owned_cookbooks
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,5 +9,19 @@ describe UsersController do
 
       expect(assigns[:user]).to eql(user)
     end
+
+    it 'assigns cookbooks' do
+      get :show, id: user.username
+
+      expect(assigns[:cookbooks]).to_not be_nil
+    end
+
+    it 'assigns a specific context of cookbooks given the tab parameter' do
+      followed_cookbook = create(:cookbook_follower, user: user).cookbook
+
+      get :show, id: user.username, tab: 'follows'
+
+      expect(assigns[:cookbooks]).to include(followed_cookbook)
+    end
   end
 end
