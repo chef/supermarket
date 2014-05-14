dfdab79bc3fb4667190efc23dd9cd3013a8ee325
Brian Cobb <bcobb@uwalumni.com>
2014-05-14 11:17:13 -0500

    Send Sidekiq errors to Sentry

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,6 @@
 if Supermarket::Config.sentry_url.present? && !Rails.env.test?
   require 'raven'
+  require 'raven/sidekiq'
 
   Raven.configure do |config|
     config.dsn = Supermarket::Config.sentry_url
