d01053aec8b5a4e542aa7240566be0c304f96660
Brian Cobb <bcobb@uwalumni.com>
2014-05-15 14:31:09 -0500

    Rescue timeout/connection errors when collecting metrics

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -8,59 +8,73 @@ class Api::V1::HealthController < Api::V1Controller
   #
   def show
     @supermarket_health = {}
-    expired_ocid_tokens = Account.
-      for('chef_oauth2').
-        where('oauth_expires < ?', Time.now).
-        count
+    @postgresql_health = { status: 'reachable' }
+    @sidekiq_health = { status: 'reachable' }
+    @redis_health = { status: 'reachable' }
 
-    @supermarket_health.store(:expired_ocid_tokens, expired_ocid_tokens)
+    begin
+      expired_ocid_tokens = Account.
+        for('chef_oauth2').
+          where('oauth_expires < ?', Time.now).
+          count
 
-    @sidekiq_health = {}
+      @supermarket_health.store(:expired_ocid_tokens, expired_ocid_tokens)
 
-    Sidekiq::Queue.new.tap do |queue|
-      @sidekiq_health.store(:latency, queue.latency)
-      @sidekiq_health.store(:queued_jobs, queue.size)
+      ActiveRecord::Base.connection.query(
+        "select count(*) from pg_stat_activity where waiting='t'"
+      ).flatten.first.to_i.tap do |waiting_on_lock|
+        @postgresql_health.store(:waiting_on_lock, waiting_on_lock)
+      end
+
+      ActiveRecord::Base.connection.query(
+        'SELECT count(*) FROM pg_stat_activity'
+      ).flatten.first.to_i.tap do |connections|
+        @postgresql_health.store(:connections, connections)
+      end
+    rescue ActiveRecord::ConnectionTimeoutError
+      @postgresql_health.store(:status, 'unknown')
+    rescue PG::ConnectionBad
+      @postgresql_health.store(:status, 'unreachable')
     end
 
-    Sidekiq::ScheduledSet.new.tap do |scheduled|
-      @sidekiq_health.store(:scheduled_jobs, scheduled.size)
-    end
+    begin
+      Sidekiq::Queue.new.tap do |queue|
+        @sidekiq_health.store(:latency, queue.latency)
+        @sidekiq_health.store(:queued_jobs, queue.size)
+      end
 
-    Sidekiq::RetrySet.new.tap do |retries|
-      @sidekiq_health.store(:retryable_jobs, retries.size)
-    end
+      Sidekiq::ScheduledSet.new.tap do |scheduled|
+        @sidekiq_health.store(:scheduled_jobs, scheduled.size)
+      end
 
-    Sidekiq::DeadSet.new.tap do |dead|
-      @sidekiq_health.store(:dead_jobs, dead.size)
-    end
+      Sidekiq::RetrySet.new.tap do |retries|
+        @sidekiq_health.store(:retryable_jobs, retries.size)
+      end
 
-    Sidekiq::Stats.new.tap do |stats|
-      @sidekiq_health.store(:total_processed, stats.processed)
-      @sidekiq_health.store(:total_failed, stats.failed)
-    end
+      Sidekiq::DeadSet.new.tap do |dead|
+        @sidekiq_health.store(:dead_jobs, dead.size)
+      end
 
-    Sidekiq::Workers.new.tap do |workers|
-      @sidekiq_health.store(:active_workers, workers.size)
-    end
+      Sidekiq::Stats.new.tap do |stats|
+        @sidekiq_health.store(:total_processed, stats.processed)
+        @sidekiq_health.store(:total_failed, stats.failed)
+      end
 
-    @postgresql_health = {}
+      Sidekiq::Workers.new.tap do |workers|
+        @sidekiq_health.store(:active_workers, workers.size)
+      end
 
-    ActiveRecord::Base.connection.query(
-      "select count(*) from pg_stat_activity where waiting='t'"
-    ).flatten.first.to_i.tap do |waiting_on_lock|
-      @postgresql_health.store(:waiting_on_lock, waiting_on_lock)
-    end
+      redis_info = Sidekiq.redis { |client| client.info }
 
-    ActiveRecord::Base.connection.query(
-      'SELECT count(*) FROM pg_stat_activity'
-    ).flatten.first.to_i.tap do |connections|
-      @postgresql_health.store(:connections, connections)
-    end
-
-    redis_info = Sidekiq.redis { |client| client.info }
-    @redis_health = %w(uptime_in_seconds connected_clients used_memory
-                       used_memory_peak).reduce({}) do |health, key|
-      health.update(key => redis_info[key].to_i)
+      %w(uptime_in_seconds connected_clients used_memory used_memory_peak).each do |key|
+        @redis_health.store(key, redis_info[key].to_i)
+      end
+    rescue Redis::TimeoutError
+      @sidekiq_health.store(:status, 'unknown')
+      @redis_health.store(:status, 'unknown')
+    rescue Redis::CannotConnectError
+      @sidekiq_health.store(:status, 'unreachable')
+      @redis_health.store(:status, 'unreachable')
     end
   end
 end
