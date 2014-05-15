71031e6d46092e2c1b7bc9c3ab7d73f02e832a14
Brian Cobb <bcobb@uwalumni.com>
2014-05-15 10:24:57 -0500

    First stab at a health API

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/api'
+
 class Api::V1::HealthController < Api::V1Controller
   #
   # GET /api/v1/health
@@ -5,9 +7,60 @@ class Api::V1::HealthController < Api::V1Controller
   # An overview of system health
   #
   def show
-    @expired_oauth_tokens = Account.
+    @supermarket_health = {}
+    expired_ocid_tokens = Account.
       for('chef_oauth2').
         where('oauth_expires < ?', Time.now).
         count
+
+    @supermarket_health.store(:expired_ocid_tokens, expired_ocid_tokens)
+
+    @sidekiq_health = {}
+
+    Sidekiq::Queue.new.tap do |queue|
+      @sidekiq_health.store(:latency, queue.latency)
+      @sidekiq_health.store(:queued_jobs, queue.size)
+    end
+
+    Sidekiq::ScheduledSet.new.tap do |scheduled|
+      @sidekiq_health.store(:scheduled_jobs, scheduled.size)
+    end
+
+    Sidekiq::RetrySet.new.tap do |retries|
+      @sidekiq_health.store(:retryable_jobs, retries.size)
+    end
+
+    Sidekiq::DeadSet.new.tap do |dead|
+      @sidekiq_health.store(:dead_jobs, dead.size)
+    end
+
+    Sidekiq::Stats.new.tap do |stats|
+      @sidekiq_health.store(:total_processed, stats.processed)
+      @sidekiq_health.store(:total_failed, stats.failed)
+    end
+
+    Sidekiq::Workers.new.tap do |workers|
+      @sidekiq_health.store(:active_workers, workers.size)
+    end
+
+    @postgresql_health = {}
+
+    ActiveRecord::Base.connection.query(
+      "select count(*) from pg_stat_activity where waiting='t'"
+    ).flatten.first.to_i.tap do |waiting_on_lock|
+      @postgresql_health.store(:waiting_on_lock, waiting_on_lock)
+    end
+
+    ActiveRecord::Base.connection.query(
+      'SELECT count(*) FROM pg_stat_activity'
+    ).flatten.first.to_i.tap do |connections|
+      @postgresql_health.store(:connections, connections)
+    end
+
+    redis_info = Sidekiq.redis { |client| client.info }
+    @redis_health = %w(uptime_in_seconds connected_clients used_memory
+                       used_memory_peak).reduce({}) do |health, key|
+      health.update(key => redis_info[key].to_i)
+    end
   end
 end
