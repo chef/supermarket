51ff0b0f66eff9643781410cd12dc695e5318571
Brian Cobb <bcobb@uwalumni.com>
2014-05-27 11:05:32 -0500

    Elaborate on the behavior of the status API

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -8,7 +8,11 @@ class Api::V1::HealthController < Api::V1Controller
   #
   # GET /api/v1/health
   #
-  # An overview of system health
+  # An overview of system health. Its HTTP status only reflects the API's
+  # ability to report status. In particular, this endpoint will return a status
+  # of 200 even if Redis and Postgres are both hosed. The response body
+  # contains information with regard to the health of individual system
+  # components.
   #
   def show
     @supermarket_health = {}
