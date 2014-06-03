c6ceb8a836a1f43b4d2a11e54d29b241d056f033
Tristan O'Neil <tristanoneil@gmail.com>
2014-06-03 15:49:58 -0400

    Improve responsiveness
    Improves the responsiveness throughout the application, specifically:
    
    - Adjust directory so columns line up on small screens.
    - Adjust cookbook header so follow button doesn't fall off.
    - Ensure cookbook sidebar has padding at the top.
    - Improve look, feel and responsiveness of welcome view.
    - Add wrapper class for welcome page.
    - Adjust edit cookbook look so there aren't so many buttons flying around.
    - Adjust font size for better readability on smaller displays.
    - Fix some minor display bugs that appear on smaller displays.
    
    Clean up stylesheets to try to adhere to code style guide.

--- app/assets/javascripts/cookbookShow.js
+++ app/assets/javascripts/cookbookShow.js
@@ -2,7 +2,7 @@ $(function() {
   $(".show-cookbook-urls-manage, .cancel-submit-urls").click(function(event) {
     event.preventDefault();
     $(".manage-cookbook-urls").slideToggle();
-    $(".show-cookbook-urls-manage").fadeToggle();
+    $(".show-cookbook-urls-manage").toggleClass('active');
     $(".cookbook-urls").fadeToggle();
   });
 
