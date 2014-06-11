2cea1ef44778e942cf29b7ff7db0a4a0510a7a31
Brian Cobb <bcobb@uwalumni.com>
2014-06-11 08:45:11 -0500

    Show Opscode Community Site metrics in our metrics endpoint

--- app/controllers/api/v1/metrics_controller.rb
+++ app/controllers/api/v1/metrics_controller.rb
@@ -1,3 +1,5 @@
+require 'supermarket/community_site'
+
 class Api::V1::MetricsController < Api::V1Controller
   #
   # GET /api/v1/metrics
@@ -12,5 +14,14 @@ class Api::V1::MetricsController < Api::V1Controller
       total_follows: CookbookFollower.count,
       total_users: User.count
     }
+
+    if ENV['COMMUNITY_SITE_DATABASE_URL'].present?
+      @metrics.update(
+        opscode_community_site_cookbook_versions: Supermarket::CommunitySite::CookbookVersionRecord.count,
+        opscode_community_site_cookbooks: Supermarket::CommunitySite::CookbookRecord.count,
+        opscode_community_site_follows: Supermarket::CommunitySite::FollowingRecord.count,
+        opscode_community_site_users: Supermarket::CommunitySite::UserRecord.count
+      )
+    end
   end
 end
