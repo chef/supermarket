2bba3d8a7b0077a24a0943ea2b4602f2ae18af1f
Tristan O'Neil <tristanoneil@gmail.com>
2014-06-16 09:58:37 -0400

    Add markdown helper specs
    Add markdown helper spec for fenced code blocks and autolinking with
    target blank.

--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -5,5 +5,20 @@ describe MarkdownHelper do
     it 'renders markdown' do
       expect(helper.render_markdown('# Test')).to eq("<h1>Test</h1>\n")
     end
+
+    it 'renders fenced code blocks' do
+      codeblock = <<-EOH
+```sh
+$ bundle exec rake spec:all
+```
+      EOH
+
+      expect(helper.render_markdown(codeblock)).to match(/<pre><code class="sh">/)
+    end
+
+    it 'auto renders links with target blank' do
+      expect(helper.render_markdown('http://getchef.com')).
+        to match(Regexp.quote('<a href="http://getchef.com" target="_blank">http://getchef.com</a>'))
+    end
   end
 end
