09a4d5ecea8f2cbbfb4ff673a5ea1b7ecc67a42d
Brian Cobb <bcobb@uwalumni.com>
2014-06-25 07:27:25 -0500

    Reset the state of the import when shutting down.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,9 @@
-Sidekiq.configure_server do
+Sidekiq.configure_server do |config|
   Supermarket::Application.load_tasks
+
+  config.on(:shutdown) do
+    ::CookbookVersion.
+      where(verification_state: 'in_progress').
+      update_all(verification_state: 'pending')
+  end
 end
