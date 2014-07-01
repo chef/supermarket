f0f2a5a765ff249ef3d2b72afff52a7208bd5613
Brett Chalupa <brett@brettchalupa.com>
2014-07-01 10:20:17 -0400

    Start working on the contributors table specs.

---
+++ spec/features/contributors_view_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_feature_helper'
+
+describe 'viewing contributors' do
+  context 'all contributors' do
+    it 'lists all folks authorized to contribute' do
+      create(:ccla_signature, organization: create(:organization, ccla_signatures_count: 0))
+      create(:icla_signature)
+
+      visit '/'
+      follow_relation 'contributors'
+
+      expect(all('.contributor').size).to eql(2)
+    end
+  end
+
+  context 'individual contributors' do
+    it 'lists all folks who have signed the ICLA' do
+      create(:ccla_signature)
+      create(:icla_signature)
+
+      visit '/'
+      follow_relation 'contributors'
+      follow_relation 'individual-contributors'
+
+      expect(all('.contributor').size).to eql(1)
+    end
+  end
+
+  context 'companies' do
+    before do
+      create(:ccla_signature)
+      create(:icla_signature)
+    end
+
+    it 'lists all companies who have signed the CCLA' do
+      visit '/'
+      follow_relation 'contributors'
+      follow_relation 'company-contributors'
+
+      expect(all('.company').size).to eql(1)
+    end
+
+    context "viewing a company's contributors" do
+      it 'lists all contributors on behalf of that company' do
+        visit '/'
+        follow_relation 'contributors'
+        follow_relation 'company-contributors'
+
+        expect(all('.company').size).to eql(1)
+      end
+    end
+  end
+
+  context 'the sidebar' do
+  end
+end
