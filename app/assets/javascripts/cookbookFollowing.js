2aca84d1310312a2c0481d046e7addd0d12f0f3b
Josh Black <raskchanky@gmail.com>
2014-07-03 15:42:24 -0700

    trackLink doesn't work with ajax

--- app/assets/javascripts/cookbookFollowing.js
+++ app/assets/javascripts/cookbookFollowing.js
@@ -1,5 +1,24 @@
 $(function() {
   /*
+   * Track following and unfollowing cookbooks in Segment.IO
+   *
+   */
+  var trackFollows = function(obj) {
+    if (window.analytics) {
+      var id = $(obj).attr('id');
+      var txt;
+
+      if (id == 'follow_cookbook') {
+        txt = 'Followed Cookbook';
+      } else {
+        txt = 'Unfollowed Cookbook';
+      }
+
+      analytics.track(txt, {cookbook: $(obj).data('cookbook')});
+    }
+  };
+
+  /*
    * Adds a disabled class when the user clicks a follow or unfollow button
    * so they know a request is in progress and they don't click follow or unfollow
    * twice.
@@ -14,6 +33,7 @@ $(function() {
    */
   $('body').delegate('.cookbook_partial .follow', 'ajax:success', function(e, data, status, xhr) {
     var cookbookId = '#' + $(this).data('cookbook');
+    trackFollows(this);
 
     $.get(window.location.href, function(response) {
       $(cookbookId).replaceWith($(response).find(cookbookId));
@@ -25,6 +45,8 @@ $(function() {
    * the followbutton which includes the follow count with server side rendred HTML.
    */
   $('body').delegate('.cookbook_show .follow', 'ajax:success', function(e, data, status, xhr) {
+    trackFollows(this);
+
     $.get(window.location.href, function(response) {
       $('.followbutton').replaceWith($(response).find('.followbutton'));
     }, 'html');
