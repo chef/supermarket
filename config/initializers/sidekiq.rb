17a08a894459490b26429bf1391df7dff160a9d7
Brian Cobb <bcobb@uwalumni.com>
2014-07-09 09:20:10 -0500

    Remove Sidekiq initializer
    It was only used for the data migration, so it is no longer needed

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-Sidekiq.configure_server do |config|
-  Supermarket::Application.load_tasks
-
-  config.on(:shutdown) do
-    ::CookbookVersion.
-      where(verification_state: 'in_progress').
-      update_all(verification_state: 'pending')
-  end
-end
