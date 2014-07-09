f3e57590a539de45cdfb78216455c5cf7027c16d
Brian Cobb <bcobb@uwalumni.com>
2014-07-09 15:01:14 -0500

    Add a rough, failing feature spec

---
+++ spec/features/ccla_membership_request_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_feature_helper'
+
+describe 'a request to join a CCLA' do
+  it 'can be accepted via email by CCLA admins' do
+    create(:ccla)
+    sign_in(create(:user))
+    sign_ccla('Acme')
+    sign_out
+
+    sign_in(create(:user))
+
+    follow_relation 'contributors'
+    follow_relation 'companies'
+    follow_relation 'contributor_request'
+
+    sign_out
+
+    request = ActionMailer::Base.deliveries.first
+    html = Nokogiri::HTML(request)
+    url = html.css("a.accept").first.attribute('href').value
+    path = URI(url).path
+
+    visit path
+
+    expect_to_see_success_message
+
+    ActionMailer::Base.deliveries.clear
+  end
+end
