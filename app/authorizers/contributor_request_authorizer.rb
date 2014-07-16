f4f8e28a987256397202c3757cd8def943afed4d
Brian Cobb <bcobb@uwalumni.com>
2014-07-16 13:21:48 -0500

    Only show the Join button to users who are able to join

--- app/authorizers/contributor_request_authorizer.rb
+++ app/authorizers/contributor_request_authorizer.rb
@@ -2,15 +2,6 @@ require 'authorizer/base'
 
 class ContributorRequestAuthorizer < Authorizer::Base
   #
-  # Only users who don't already belong to the organization can make requests
-  #
-  # @return [Boolean]
-  #
-  def create?
-    record.organization.contributors.where(user_id: user.id).empty?
-  end
-
-  #
   # Only admins of the organization which the requestor would like to join may
   # accept a request to join that organization
   #
