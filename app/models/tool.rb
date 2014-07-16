7edcf1e4c3ec2a5412d5b9bc426cca32ac99f19c
Josh Black <raskchanky@gmail.com>
2014-07-16 08:53:04 -0700

    validate tool names in a case-insensitive way

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,5 +1,49 @@
 class Tool < ActiveRecord::Base
   self.inheritance_column = nil
 
+  # Associations
+  # --------------------
   belongs_to :user
+
+  # Validations
+  # --------------------
+  validates :name, uniqueness: { case_sensitive: false }
+
+  # Callbacks
+  # --------------------
+  before_validation :copy_name_to_lowercase_name
+
+  #
+  # Query tools by case-insensitive name.
+  #
+  # @param name [String, Array<String>] a single name, or a collection of names
+  #
+  # @example
+  #   Tool.with_name('dingus').first
+  #     #<Tool name: "dingus"...>
+  #   Tool.with_name(['dingus', 'thingy']).to_a
+  #     [#<Tool name: "dingus"...>, #<Tool name: "thingy"...>]
+  #
+  # @todo: query and index by +LOWER(name)+ when ruby schema dumps support such
+  #   a thing.
+  #
+  scope :with_name, lambda { |names|
+    lowercase_names = Array(names).map { |name| name.to_s.downcase }
+
+    where(lowercase_name: lowercase_names)
+  }
+
+  private
+
+  #
+  # Populates the +lowercase_name+ attribute with the lowercase +name+
+  #
+  # This exists until Rails schema dumping supports Posgres's expression
+  # indices, which would allow us to create an index on LOWER(name). To do that
+  # now, we'd have to use the raw SQL schema dumping functionality, which is
+  # less-than ideal
+  #
+  def copy_name_to_lowercase_name
+    self.lowercase_name = name.to_s.downcase
+  end
 end
