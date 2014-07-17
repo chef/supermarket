959fa6f4227bb016e27438a05e9bd4b8cddb740f
Brett Chalupa <brett@brettchalupa.com>
2014-07-17 11:51:12 -0400

    Add UI for cookbook deprecation

---
+++ app/assets/javascripts/cookbookDeprecate.js
@@ -0,0 +1,31 @@
+$(document).on('opened', '[data-reveal]', function () {
+  var settings =  {
+    placeholder: 'Search for a cookbook',
+    minimumInputLength: 3,
+    width: '100%',
+    ajax: {
+      url: function () {
+        return $(this).data('url');
+      },
+      dataType: 'json',
+      quietMillis: 200,
+      data: function (term, page) {
+        return { q: term };
+      },
+      results: function (data, page) {
+        return { results: data.items };
+      },
+    },
+    id: function(object) {
+      return object.cookbook_name;
+    },
+    formatSelection: function(object, container) {
+      return object.cookbook_name;
+    },
+    formatResult: function(object, container) {
+      return object.cookbook_name;
+    }
+  }
+
+  $('.cookbook-deprecate').select2(settings);
+});
