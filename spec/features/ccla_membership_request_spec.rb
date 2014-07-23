d49e8c8e3c15dda37b94d24251c25a53383f0fa8
Brett Chalupa <brettchalupa@gmail.com>
2014-07-23 15:26:50 -0400

    Add the wait_for helper to feature_helpers.rb

--- spec/features/ccla_membership_request_spec.rb
+++ spec/features/ccla_membership_request_spec.rb
@@ -1,21 +1,6 @@
 require 'spec_feature_helper'
 
 describe 'a request to join a CCLA' do
-  #
-  # Wait about five seconds for a condition to be true
-  #
-  def wait_for(&condition)
-    ticks = 0
-
-    loop do
-      sleep 1
-      ticks += 1
-
-      break if ticks > 5
-      break if condition.call
-    end
-  end
-
   it 'can be accepted via email by CCLA admins', use_poltergeist: true do
     admin_user = create(:user)
     create(:ccla)
