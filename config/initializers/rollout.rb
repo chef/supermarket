189fa5224df3d7f68097e63b84f3c3fc3511e9a1
Josh Black <raskchanky@gmail.com>
2014-10-10 09:19:53 -0700

    initialize ROLLOUT lazily

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -1,24 +1,32 @@
-require 'redis'
+def Object.const_missing(const)
+  if const == :ROLLOUT
+    require 'redis'
 
-redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/0/supermarket'
+    redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/0/supermarket'
 
-redis = Redis.new(url: redis_url)
-ROLLOUT = Rollout.new(redis)
+    redis = Redis.new(url: redis_url)
+    Object.const_set('ROLLOUT', Rollout.new(redis))
 
-features = ENV['FEATURES'].to_s.split(',')
+    features = ENV['FEATURES'].to_s.split(',')
 
-#
-# Features that are defined in rollout but are no longer defined
-# in ENV['FEATURES'] need to be deactivated.
-#
-(ROLLOUT.features - features).each do |feature|
-  ROLLOUT.deactivate(feature)
-end
+    #
+    # Features that are defined in rollout but are no longer defined
+    # in ENV['FEATURES'] need to be deactivated.
+    #
+    (ROLLOUT.features - features).each do |feature|
+      ROLLOUT.deactivate(feature)
+    end
 
-#
-# Features that are defined in ENV['FEATURES'] but are
-# not defined in rollout need to be activated.
-#
-(features - ROLLOUT.features).each do |feature|
-  ROLLOUT.activate(feature)
+    #
+    # Features that are defined in ENV['FEATURES'] but are
+    # not defined in rollout need to be activated.
+    #
+    (features - ROLLOUT.features).each do |feature|
+      ROLLOUT.activate(feature)
+    end
+
+    ROLLOUT
+  else
+    super
+  end
 end
