41e3332b570e0f8c03e480557384e49ed0c68e38
Nathan L Smith <smith@getchef.com>
2014-10-14 21:14:54 -0500

    clean up some attribute formatting

--- cookbooks/omnibus-supermarket/attributes/default.rb
+++ cookbooks/omnibus-supermarket/attributes/default.rb
@@ -1,9 +1,18 @@
-# Supermarket configuration
+# # Supermarket configuration
 #
-# Attributes here will be applied to configure the application.
+# Attributes here will be applied to configure the application and the services
+# it uses.
+#
+# Most of the attributes in this file are things you will not need to ever
+# touch, but they are here in case you need them.
 
-# Top-level attributes
-######################
+# ## Common Use Cases
+#
+# These are examples of things you may want to do, depending on how you set up
+# the application to run.
+# TODO
+
+# ## Top-level attributes
 #
 # These are used by the other items below. More app-specific top-level
 # attributes are further down in this file.
@@ -11,6 +20,7 @@
 # The fully qualified domain name. Will use the node's fqdn if nothing is
 # specified.
 default['supermarket']['fqdn'] = node['fqdn']
+
 default['supermarket']['config_directory'] = '/etc/supermarket'
 default['supermarket']['install_directory'] = '/opt/supermarket'
 default['supermarket']['app_directory'] = "#{node['supermarket']['install_directory']}/embedded/service/supermarket"
@@ -19,22 +29,22 @@ default['supermarket']['var_directory'] = '/var/opt/supermarket'
 default['supermarket']['user'] = 'supermarket'
 default['supermarket']['group'] = 'supermarket'
 
-# Enterprise
-############
+# ## Enterprise
 #
 # The "enterprise" cookbook provides recipes and resources we can use for this
 # app.
 
 default['enterprise']['name'] = 'supermarket'
+
 # Enterprise uses install_path internally, but we use install_directory because
 # it's more consistent. Alias it here so both work.
 default['supermarket']['install_path'] = node['supermarket']['install_directory']
+
 # An identifier used in /etc/inittab (default is 'SV'). Needs to be a unique
 # (for the file) sequence of 1-4 characters.
 default['supermarket']['sysvinit_id'] = 'SUP'
 
-# Nginx
-#######
+# ## Nginx
 
 default['supermarket']['nginx']['enable'] = true
 default['supermarket']['nginx']['directory'] = "#{node['supermarket']['var_directory']}/nginx"
@@ -59,7 +69,7 @@ default['nginx']['gzip_comp_level'] = '2'
 default['nginx']['gzip_proxied'] = 'any'
 default['nginx']['gzip_vary'] = 'off'
 default['nginx']['gzip_buffers'] = nil
-default['nginx']['gzip_types'] = %w(
+default['nginx']['gzip_types'] = %w[
   text/plain
   text/css
   application/x-javascript
@@ -70,10 +80,9 @@ default['nginx']['gzip_types'] = %w(
   text/javascript
   application/javascript
   application/json
-)
+]
 default['nginx']['gzip_min_length'] = 1000
 default['nginx']['gzip_disable'] = 'MSIE [1-6]\.'
-
 default['nginx']['keepalive'] = 'on'
 default['nginx']['keepalive_timeout'] = 65
 default['nginx']['worker_processes'] = node['cpu'] && node['cpu']['total'] ? node['cpu']['total'] : 1
@@ -90,23 +99,23 @@ default['nginx']['disable_access_log'] = false
 default['nginx']['default_site_enabled'] = false
 default['nginx']['types_hash_max_size'] = 2048
 default['nginx']['types_hash_bucket_size'] = 64
-
 default['nginx']['proxy_read_timeout'] = nil
 default['nginx']['client_body_buffer_size'] = nil
 default['nginx']['client_max_body_size'] = '250m'
 default['nginx']['default']['modules'] = []
 
-# Postgres
-##########
+# ## Postgres
 
 default['supermarket']['postgresql']['enable'] = true
 default['supermarket']['postgresql']['username'] = node['supermarket']['user']
 default['supermarket']['postgresql']['data_directory'] = "#{node['supermarket']['var_directory']}/postgresql/9.3/data"
-# Logs
+
+# ### Logs
 default['supermarket']['postgresql']['log_directory'] = "#{node['supermarket']['log_directory']}/postgresql"
 default['supermarket']['postgresql']['log_rotation']['file_maxbytes'] = 104857600
 default['supermarket']['postgresql']['log_rotation']['num_to_keep'] = 10
-# DB settings
+
+# ### DB settings
 default['supermarket']['postgresql']['checkpoint_completion_target'] = 0.5
 default['supermarket']['postgresql']['checkpoint_segments'] = 3
 default['supermarket']['postgresql']['checkpoint_timeout'] = '5min'
@@ -121,8 +130,7 @@ default['supermarket']['postgresql']['shmmax'] = 17179869184
 default['supermarket']['postgresql']['shmall'] = 4194304
 default['supermarket']['postgresql']['work_mem'] = "8MB"
 
-# Rails
-#######
+# ## Rails
 #
 # The Rails app for Supermarket
 default['supermarket']['rails']['enable'] = true
@@ -131,9 +139,7 @@ default['supermarket']['rails']['log_directory'] = "#{node['supermarket']['log_d
 default['supermarket']['rails']['log_rotation']['file_maxbytes'] = 104857600
 default['supermarket']['rails']['log_rotation']['num_to_keep'] = 10
 
-# Redis
-#######
-
+# ## Redis
 default['supermarket']['redis']['enable'] = true
 default['supermarket']['redis']['bind'] = '127.0.0.1'
 default['supermarket']['redis']['directory'] = "#{node['supermarket']['var_directory']}/redis"
@@ -142,15 +148,13 @@ default['supermarket']['redis']['log_rotation']['file_maxbytes'] = 104857600
 default['supermarket']['redis']['log_rotation']['num_to_keep'] = 10
 default['supermarket']['redis']['port'] = 16379
 
-# Runit
-#######
+# ## Runit
 
 # This is missing from the enterprise cookbook
 # see (https://github.com/opscode-cookbooks/enterprise-chef-common/pull/17)
 default['runit']['svlogd_bin'] = "#{node['supermarket']['install_directory']}/embedded/bin/svlogd"
 
-# Sidekiq
-#########
+# ## Sidekiq
 #
 # Used for background jobs
 default['supermarket']['sidekiq']['enable'] = true
@@ -160,9 +164,7 @@ default['supermarket']['sidekiq']['log_rotation']['file_maxbytes'] = 104857600
 default['supermarket']['sidekiq']['log_rotation']['num_to_keep'] = 10
 default['supermarket']['sidekiq']['timeout'] = 30
 
-# SSL
-#####
-
+# ## SSL
 default['supermarket']['ssl']['directory'] = '/var/opt/supermarket/ssl'
 
 # This shouldn't be changed, but can be overriden in tests
@@ -181,8 +183,7 @@ default['supermarket']['ssl']['company_name'] = "My Supermarket"
 default['supermarket']['ssl']['organizational_unit_name'] = "Operations"
 default['supermarket']['ssl']['email_address'] = "you@example.com"
 
-# Unicorn
-#########
+# ## Unicorn
 #
 # Settings for main Rails app Unicorn application server. These attributes are
 # used with the template from the community Unicorn cookbook:
@@ -190,6 +191,7 @@ default['supermarket']['ssl']['email_address'] = "you@example.com"
 #
 # Full explanation of all options can be found at
 # http://unicorn.bogomips.org/Unicorn/Configurator.html
+
 default['supermarket']['unicorn']['name'] = 'supermarket'
 default['supermarket']['unicorn']['copy_on_write'] = true
 default['supermarket']['unicorn']['enable_stats'] = false
@@ -213,8 +215,7 @@ default['supermarket']['unicorn']['working_directory'] = nil
 default['supermarket']['unicorn']['before_fork'] = nil
 default['supermarket']['unicorn']['after_fork'] = nil
 
-# Database
-##########
+# ## Database
 
 default['supermarket']['database']['user'] = node['supermarket']['postgresql']['username']
 default['supermarket']['database']['name'] = 'supermarket'
@@ -222,10 +223,10 @@ default['supermarket']['database']['host'] = node['supermarket']['postgresql']['
 default['supermarket']['database']['port'] = node['supermarket']['postgresql']['port']
 default['supermarket']['database']['pool'] = node['supermarket']['sidekiq']['concurrency']
 
-# App-specific top-level attributes
-###################################
+# ## App-specific top-level attributes
 #
 # These are used by Rails and Sidekiq. Most will be exported directly to
 # environment variables to be used by the app.
+
 default['supermarket']['redis_url'] = "redis://#{node['supermarket']['redis']['bind']}:#{node['supermarket']['redis']['port']}/0/supermarket"
 default['newrelic_agent_enabled'] = 'false'
