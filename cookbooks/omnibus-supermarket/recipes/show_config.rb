70847867eb0c955d5858c1bf6abcb4c437d1fa0d
Nathan L Smith <smith@getchef.com>
2014-10-17 12:29:14 -0500

    use chef software inc for the copyright

--- cookbooks/omnibus-supermarket/recipes/show_config.rb
+++ cookbooks/omnibus-supermarket/recipes/show_config.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: supermarket
 # Recipe:: show_config
 #
-# Copyright 2014 Chef Supermarket Team
+# Copyright 2014 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
