3d27243d7d1a333b0c8fe5c3f4c30166f1f18c5a
Seth Chisamore <schisamo@getchef.com>
2014-10-20 19:29:59 -0400

    Update Omnibus config with sane defaults
    * Use S3 caching
    * Enable build retries
    * Bump fetcher timeout

--- omnibus.rb
+++ omnibus.rb
@@ -30,15 +30,16 @@
 
 # Enable S3 asset caching
 # ------------------------------
-# use_s3_caching true
-# s3_access_key  ENV['AWS_ACCESS_KEY_ID']
-# s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
-# s3_bucket      ENV['AWS_S3_BUCKET']
+use_s3_caching true
+s3_access_key  ENV['AWS_ACCESS_KEY_ID']
+s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
+s3_bucket      ENV['AWS_S3_BUCKET'] || 'opscode-omnibus-cache'
 
 # Customize compiler bits
 # ------------------------------
-# solaris_compiler 'gcc'
-# build_retries 5
+solaris_compiler 'gcc'
+build_retries 5
+fetcher_read_timeout 120
 
 # Load additional software
 # ------------------------------
