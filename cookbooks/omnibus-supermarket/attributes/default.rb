066fd23b676bc0b7b17156ffa8e1be8d63287f6c
Nathan L Smith <smith@getchef.com>
2014-10-21 21:27:58 -0500

    add comment about GitHub feature

--- cookbooks/omnibus-supermarket/attributes/default.rb
+++ cookbooks/omnibus-supermarket/attributes/default.rb
@@ -386,6 +386,7 @@ default['supermarket']['seed_cla_data'] = nil
 # * cla: Enable the Contributor License Agreement features
 # * fieri: Use the fieri service to report on cookbook quality (requires
 #   fieri_url and fieri_key to be set.)
+# * github: Enable GitHub integration, used with CLA signing
 # * join_ccla: Enable joining of Corporate CLAs
 # * tools: Enable the tools section
 default['supermarket']['features'] = 'tools'
