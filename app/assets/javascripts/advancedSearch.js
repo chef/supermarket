6644250403858e4cf4661c5b1d029ec864230992
Nell Shamrell <nellshamrell@gmail.com>
2015-05-04 14:53:06 -0700

    refining styling and appearance

--- app/assets/javascripts/advancedSearch.js
+++ app/assets/javascripts/advancedSearch.js
@@ -4,13 +4,18 @@ $(function() {
   }
 
   $(".advanced_search_toggle span").click(function() {
+
     if ($(".advanced_search_body").is(":hidden")) {
       $(".advanced_search_body").slideDown(300);
       $.cookie('advancedSearch' ,'on')
+      $("#toggle-arrow").removeClass("fa-chevron-down");
+      $("#toggle-arrow").addClass("fa-chevron-up");
     } else {
       $(".advanced_search_body").slideUp(240);
       $.cookie('advancedSearch', 'off');
       $('input:checkbox').removeAttr('checked');
+      $("#toggle-arrow").removeClass("fa-chevron-up");
+      $("#toggle-arrow").addClass("fa-chevron-down");
     }
   });
 });
