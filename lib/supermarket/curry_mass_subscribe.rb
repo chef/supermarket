267a031aa3c11dcf7872a033be10a0fd687f864a
Nell Shamrell <nellshamrell@gmail.com>
2015-09-24 12:12:17 -0700

    refactors retrieving the org repos into a private method
    wip
    
    wip
    
    wip

--- lib/supermarket/curry_mass_subscribe.rb
+++ lib/supermarket/curry_mass_subscribe.rb
@@ -2,15 +2,32 @@ module Supermarket
   require 'Octokit'
 
   class CurryMassSubscribe
+    include ActionView::Helpers
+    include ActionDispatch::Routing
+    include Rails.application.routes.url_helpers
 
     def subscribe_org_repos(org)
-      client.org_repos(org)
+      public_repos(org).each do |repo|
+        Curry::Repository.where(owner: org, name: repo[:name])
+        repository = Curry::Repository.new(owner: org, name: repo[:name])
+        subscriber = Curry::RepositorySubscriber.new(repository)
+
+        if subscriber.subscribe('https://supermarket.chef.io/curry/pull_request_updates')
+          Curry::RepositorySubscriptionWorker.perform_async(repository.id)
+        else
+          puts "Unable to subscribe #{repository.name}"
+        end
+      end
     end
 
     private
 
     def client
       Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+    end
+
+    def public_repos(org)
+      client.org_repos(org, private: true)
     end
   end
 end
--- spec/lib/supermarket/curry_mass_subscribe_spec.rb
+++ spec/lib/supermarket/curry_mass_subscribe_spec.rb
@@ -32,7 +32,7 @@ describe Supermarket::CurryMassSubscribe do
       end
 
       it 'finds all public repos for the organization' do
-        expect(client).to receive(:org_repos).with(sample_org)
+        expect(client).to receive(:org_repos).with(sample_org, private: true).and_return([])
         curry_mass_subscribe.subscribe_org_repos(sample_org)
       end
     end
@@ -58,12 +58,86 @@ describe Supermarket::CurryMassSubscribe do
         allow(client).to receive(:org_repos).and_return(sample_org_public_repos)
       end
 
-      it 'checks whether the repo is already monitored by Curry' do
+      it 'iterates over the public repos' do
+        expect(sample_org_public_repos).to receive(:each)
+        curry_mass_subscribe.subscribe_org_repos(sample_org)
+      end
 
+      it 'checks whether the repo is already monitored by Curry' do
+        allow(client).to receive(:org_repos).and_return([sample_org_public_repos.first])
+        expect(Curry::Repository).to receive(:where)
+        .with(owner: sample_org, name: sample_org_public_repos.first[:name])
+
+        curry_mass_subscribe.subscribe_org_repos(sample_org)
       end
 
       context 'when the repository is not already monitored by Curry' do
+        let(:curry_repository) do
+          Curry::Repository.new(owner: sample_org, name: 'Some Name', callback_url: 'http://example.com')
+        end
 
+        let(:repository_subscriber) { Curry::RepositorySubscriber.new(curry_repository) }
+
+        before do
+          allow(Curry::Repository).to receive(:where).and_return([])
+          allow(Curry::Repository).to receive(:new).and_return(curry_repository)
+          allow(client).to receive(:org_repos).and_return([sample_org_public_repos.first])
+          allow(Curry::RepositorySubscriber).to receive(:new).and_return(repository_subscriber)
+        end
+
+        it 'creates a new repository record' do
+          expect(Curry::Repository).to receive(:new).with(owner: sample_org, name: sample_org_public_repos.first[:name])
+
+          curry_mass_subscribe.subscribe_org_repos(sample_org)
+        end
+
+        it 'creates a new repository subscriber object' do
+          expect(Curry::RepositorySubscriber).to receive(:new).with(curry_repository)
+          curry_mass_subscribe.subscribe_org_repos(sample_org)
+        end
+
+        it 'subscribes the repository' do
+          allow(Curry::RepositorySubscriber).to receive(:new).and_return(repository_subscriber)
+          expect(repository_subscriber).to receive(:subscribe)
+          curry_mass_subscribe.subscribe_org_repos(sample_org)
+        end
+
+        it 'includes the curry_pull_request_updates_url' do
+          expect(repository_subscriber).to receive(:subscribe).with('https://supermarket.chef.io/curry/pull_request_updates')
+          curry_mass_subscribe.subscribe_org_repos(sample_org)
+        end
+
+        context 'when the subscribe is successful' do
+          before do
+            allow(Curry::RepositorySubscriber).to receive(:new).and_return(repository_subscriber)
+            allow(repository_subscriber).to receive(:subscribe).and_return(true)
+          end
+
+          it 'creates a subscription worker' do
+            # Save the curry worker so we can get the id
+            curry_repository.save!
+
+            expect(Curry::RepositorySubscriptionWorker).to receive(:perform_async).with(curry_repository.id)
+            curry_mass_subscribe.subscribe_org_repos(sample_org)
+          end
+
+        end
+
+        context 'when the subscribe is not successful' do
+          before do
+            allow(Curry::RepositorySubscriber).to receive(:new).and_return(repository_subscriber)
+            allow(repository_subscriber).to receive(:subscribe).and_return(false)
+          end
+
+          it 'does not create a subscription worker' do
+            expect(Curry::RepositorySubscriptionWorker).to_not receive(:perform_async)
+            curry_mass_subscribe.subscribe_org_repos(sample_org)
+          end
+
+          it 'displays an error message' do
+            expect { curry_mass_subscribe.subscribe_org_repos(sample_org) }.to output("Unable to subscribe #{curry_repository.name}\n").to_stdout
+          end
+        end
       end
 
 
