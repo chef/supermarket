287cb3450058ac26e0a71dde401ebbd8fd102dc6
Nell Shamrell <nellshamrell@gmail.com>
2015-10-22 11:05:25 -0700

    adds in code and specs to collaborator processing concern to find the correct collaborator resource

--- app/controllers/concerns/collaborator_processing.rb
+++ app/controllers/concerns/collaborator_processing.rb
@@ -1,3 +1,15 @@
 module CollaboratorProcessing
   extend ActiveSupport::Concern
+
+  included do
+    helper_method :add_users_as_collaborators
+  end
+
+  def add_users_as_collaborators(resourceable_type, resourceable_id)
+    resource = resourceable_type.constantize.find(
+      resourceable_id
+    )
+  end
+
+
 end
--- spec/controllers/concerns/collaborator_processing_spec.rb
+++ spec/controllers/concerns/collaborator_processing_spec.rb
@@ -5,5 +5,32 @@ class FakesController < ApplicationController
 end
 
 describe FakesController do
+  let(:cookbook) { create(:cookbook) }
 
+  context 'finding the resource' do
+    context 'when the resource is a cookbook' do
+      it 'finds the correct cookbook' do
+        expect(Cookbook).to receive(:find).with(cookbook.id).and_return(cookbook)
+        subject.add_users_as_collaborators('Cookbook', cookbook.id)
+      end
+    end
+
+    context 'when the resource is a tool' do
+      let(:tool) { create(:tool) }
+
+      it 'finds the correct tool' do
+        expect(Tool).to receive(:find).with(tool.id).and_return(tool)
+        subject.add_users_as_collaborators('Tool', tool.id)
+      end
+    end
+  end
+
+  context 'finding users' do
+    context 'finding non-eligible user ids' do
+#      it 'finds non-eligible users' do
+#        expect(Collaborator).to receive(:ineligible_collaborators_for).with(cookbook)
+#        subject.add_users_as_collaborators
+#      end
+    end
+  end
 end
