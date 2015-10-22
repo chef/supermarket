7cc4b6e9c0527c7b8c8107088252e4d951ea6d0f
Nell Shamrell <nellshamrell@gmail.com>
2015-10-22 11:35:31 -0700

    adds code and specs to find the correct users to add as contributors

--- app/controllers/concerns/collaborator_processing.rb
+++ app/controllers/concerns/collaborator_processing.rb
@@ -5,11 +5,25 @@ module CollaboratorProcessing
     helper_method :add_users_as_collaborators
   end
 
-  def add_users_as_collaborators(resourceable_type, resourceable_id)
+  def add_users_as_collaborators(resourceable_type, resourceable_id, user_ids)
     resource = resourceable_type.constantize.find(
       resourceable_id
     )
+
+    Collaborator.ineligible_collaborators_for(resource).map(&:id).map(&:to_s)
+
+    user_ids = user_ids.split(',') - ineligible_ids(resource)
+    User.where(id: user_ids)
+
   end
 
+  private
 
+  def ineligible_ids(resource)
+    if Collaborator.ineligible_collaborators_for(resource)
+      Collaborator.ineligible_collaborators_for(resource).map(&:id).map(&:to_s)
+    else
+      []
+    end
+  end
 end
--- spec/controllers/concerns/collaborator_processing_spec.rb
+++ spec/controllers/concerns/collaborator_processing_spec.rb
@@ -6,12 +6,22 @@ end
 
 describe FakesController do
   let(:cookbook) { create(:cookbook) }
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+  let(:user3) { create(:user) }
+
+  let(:users) {[user1, user2, user3]}
+  let(:user_ids) { "#{user1.id},#{user2.id},#{user3.id}" }
+
+  before do
+    allow(subject).to receive(:current_user) { create(:user) }
+  end
 
   context 'finding the resource' do
     context 'when the resource is a cookbook' do
       it 'finds the correct cookbook' do
         expect(Cookbook).to receive(:find).with(cookbook.id).and_return(cookbook)
-        subject.add_users_as_collaborators('Cookbook', cookbook.id)
+        subject.add_users_as_collaborators('Cookbook', cookbook.id, user_ids)
       end
     end
 
@@ -20,17 +30,48 @@ describe FakesController do
 
       it 'finds the correct tool' do
         expect(Tool).to receive(:find).with(tool.id).and_return(tool)
-        subject.add_users_as_collaborators('Tool', tool.id)
+        subject.add_users_as_collaborators('Tool', tool.id, user_ids)
       end
     end
   end
 
-  context 'finding users' do
+  context 'adding users' do
     context 'finding non-eligible user ids' do
-#      it 'finds non-eligible users' do
-#        expect(Collaborator).to receive(:ineligible_collaborators_for).with(cookbook)
-#        subject.add_users_as_collaborators
-#      end
+
+      before do
+        allow(Collaborator).to receive(:ineligible_collaborators_for).and_return(users)
+      end
+
+      it 'finds non-eligible users' do
+        expect(Collaborator).to receive(:ineligible_collaborators_for).with(cookbook)
+        subject.add_users_as_collaborators('Cookbook', cookbook.id, user_ids)
+      end
+
+      it 'maps the user ids' do
+        expect(users).to receive(:map).and_return([user1.id, user2.id, user3.id])
+        subject.add_users_as_collaborators('Cookbook', cookbook.id, user_ids)
+      end
+
+      it 'converts the ids to strings' do
+        id_array = [user1.id,user2.id,user3.id]
+
+        allow(users).to receive(:map).and_return(id_array)
+        expect(id_array).to receive(:map).and_return(user_ids)
+        subject.add_users_as_collaborators('Cookbook', cookbook.id, user_ids)
+      end
+    end
+
+    context 'finding users' do
+      it 'finds all eligible users' do
+        expect(User).to receive(:where).with(id: ["#{user1.id}","#{user2.id}","#{user3.id}"]).and_return(users)
+        subject.add_users_as_collaborators('Cookbook', cookbook.id, user_ids)
+      end
+
+      it 'does not include non-eligible users' do
+        allow(Collaborator).to receive(:ineligible_collaborators_for).and_return([user2])
+        expect(User).to receive(:where).with(id: ["#{user1.id}","#{user3.id}"]).and_return([user1,user2])
+        subject.add_users_as_collaborators('Cookbook', cookbook.id, user_ids)
+      end
     end
   end
 end
