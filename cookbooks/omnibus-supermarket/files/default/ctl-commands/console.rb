cb86efb2ae6de9698e9b948822d874ed0860cd5e
Nell Shamrell <nellshamrell@gmail.com>
2015-12-21 15:26:05 -0800

    runs console command as supermarket user rather than root

--- cookbooks/omnibus-supermarket/files/default/ctl-commands/console.rb
+++ cookbooks/omnibus-supermarket/files/default/ctl-commands/console.rb
@@ -1,5 +1,5 @@
 add_command 'console', 'Enter the rails console for Supermarket', 1 do
-  cmd = "cd /opt/supermarket/embedded/service/supermarket && sudo env PATH=/opt/supermarket/embedded/bin bin/rails console production"
+  cmd = "cd /opt/supermarket/embedded/service/supermarket && sudo -u supermarket env PATH=/opt/supermarket/embedded/bin bin/rails console production"
   exec cmd
   true
 end
