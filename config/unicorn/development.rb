92f0f8ec50ab25c6b58794cf0f906cbdd51836bd
Robb Kidd <rkidd@chef.io>
2016-02-03 16:13:49 -0500

    Configure Rubocop to run the Rails recommendations no matter how run.
    Omit the puts output lines from dev unicorn config because I can't figure
    out how to get the Rails logger in there (or if it is even possible or
    appropriate).

--- config/unicorn/development.rb
+++ config/unicorn/development.rb
@@ -4,7 +4,6 @@ preload_app true
 
 before_fork do |_server, _worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
@@ -14,7 +13,7 @@ end
 
 after_fork do |_server, _worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+    # 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) &&
