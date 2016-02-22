4ec5f81c18b340b9c9cc07aaa230425ad41ce7c9
Nell Shamrell <nellshamrell@gmail.com>
2016-02-22 15:33:44 -0800

    adds code to submit the correct tarball url to fieri when a Supermarket stores cookbooks on the local disk rather than S3
    adds code to submit the correct tarball url to fieri when a Supermarket stores cookbooks on the local disk rather than S3

--- spec/workers/fieri_notify_worker_spec.rb
+++ spec/workers/fieri_notify_worker_spec.rb
@@ -12,4 +12,49 @@ describe FieriNotifyWorker do
 
     expect(result.class).to eql(Net::HTTPOK)
   end
+
+  context 'setting the correct cookbook artifact url' do
+    let(:version) { create(:cookbook_version, cookbook: cookbook) }
+
+    before do
+      allow(CookbookVersion).to receive(:find).and_return(version)
+    end
+
+    context 'when not using S3 for cookbook storage' do
+      before do
+        ENV['S3_BUCKET'] = nil
+      end
+
+      it 'includes the correct cookbook artifact url' do
+        expect(Net::HTTP).to receive(:post_form).with(anything, hash_including("cookbook_artifact_url" => "#{Supermarket::Host.full_url}#{version.tarball.url}"))
+
+        worker = FieriNotifyWorker.new
+        worker.perform(version.id)
+      end
+    end
+
+    context 'when using S3 for cookbook storage' do
+      before do
+        ENV['S3_BUCKET'] = 'mybucket'
+        ENV['S3_ACCESS_KEY_ID'] = '123'
+        ENV['S3_SECRET_ACCESS_KEY'] = '456'
+
+        # Paths for cookbooks are configured in config/initializers/paperclip.rb
+        # These variables are set to simulate cookbooks which are configured to
+        # be stored on S3
+        default_s3_url = "https://s3.amazonaws.com/"
+        s3_path = version.tarball.url.sub(%r{^/system}, '') # S3 cookbook paths do not have /system at the beginning of them
+
+        s3_tarball_url = "#{default_s3_url}#{ENV['S3_BUCKET']}#{s3_path}"
+
+        allow(version).to receive_message_chain(:tarball, :url).and_return(s3_tarball_url)
+      end
+
+      it 'includes the correct cookbook artifact url' do
+        expect(Net::HTTP).to receive(:post_form).with(anything, hash_including("cookbook_artifact_url" => version.tarball.url.to_s))
+        worker = FieriNotifyWorker.new
+        worker.perform(version.id)
+      end
+    end
+  end
 end
