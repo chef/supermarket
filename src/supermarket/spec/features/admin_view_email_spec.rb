e720dd8fa06c318096b0157f209791db502dd447
Logan McDonald <loganmcdona11@gmail.com>
2016-04-20 13:48:46 -0700

    added feature spec for admins ability to view email of user

---
+++ src/supermarket/spec/features/admin_view_email_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'admin email permissions' do
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
+
+  scenario 'admin user goes to another users page' do
+    sign_in(admin)
+    visit user_path(user)
+    follow_relation 'make_admin'
+
+    expect(page).to have_content(user.email)
+  end
+
+  scenario 'regular user goes to another users page' do
+    sign_in(user)
+    visit user_path(user)
+
+    expect(page).to_not have_content(user.email)
+  end
+end
