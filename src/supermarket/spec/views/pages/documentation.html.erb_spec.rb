d83537918439c0617efc9d8fb5e68625e38fb945
Logan McDonald <loganmcdona11@gmail.com>
2016-04-21 13:42:08 -0700

    refactors spec and checks that links are displayed

--- src/supermarket/spec/views/pages/documentation.html.erb_spec.rb
+++ src/supermarket/spec/views/pages/documentation.html.erb_spec.rb
@@ -1,12 +1,10 @@
 require "spec_helper"
 
 describe "pages/documentation.html.erb" do
-
-  context "with 2 pages" do
+  context "page with links" do
     it "displays links" do
       render
-
-      expect(rendered).to have_link 'Supermarket Docs', href: 'https://docs.chef.io/supermarket.html'
+      expect(rendered).to have_selector('a[href]')
     end
   end
 end
