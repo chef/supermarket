43546cc629c6dd4b7eea0f6b62373f45e439d126
Logan McDonald <loganmcdona11@gmail.com>
2016-04-21 10:47:53 -0700

    changes the new announcments to a more information tab that is reactive when clicked

--- src/supermarket/app/assets/javascripts/announcementBanner.js
+++ src/supermarket/app/assets/javascripts/announcementBanner.js
@@ -1,15 +1,14 @@
-var cancelIsClicked = false;
+// var cancelIsClicked = false;
 
-$("span.fa.fa-times").click(function(){
-  cancelIsClicked = true;
-});
+// $(".fa.fa-times").click(function(){
+//   cancelIsClicked = true;
+// });
+
 
 $(function() {
-  $(".announcement_banner_header").click(function() {
-    if (cancelIsClicked) {
-      $(".announcement_banner_header").hide();
-    } else if($(".announcement_banner_content").is(":hidden")) {
-      $(".announcement_banner_content").slideDown(300);
+  $(".more-info").click(function() {
+    if ($(".announcement_banner_content").is(":hidden")) {
+      $(".announcement_banner_content").slideDown(50);
     } else {
       $(".announcement_banner_content").slideUp(240);
     }
