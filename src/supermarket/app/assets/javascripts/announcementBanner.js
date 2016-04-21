93181b7ae74f64ad2ed63d0bd8d2d56883f5873c
Logan McDonald <loganmcdona11@gmail.com>
2016-04-21 07:28:48 -0700

    added html simple cancel button and javascript behavioral logic to hide the banner when clicked

--- src/supermarket/app/assets/javascripts/announcementBanner.js
+++ src/supermarket/app/assets/javascripts/announcementBanner.js
@@ -1,6 +1,14 @@
+var cancelIsClicked = false;
+
+$("span.fa.fa-times").click(function(){
+  cancelIsClicked = true;
+});
+
 $(function() {
   $(".announcement_banner_header").click(function() {
-    if ($(".announcement_banner_content").is(":hidden")) {
+    if (cancelIsClicked) {
+      $(".announcement_banner_header").hide();
+    } else if($(".announcement_banner_content").is(":hidden")) {
       $(".announcement_banner_content").slideDown(300);
     } else {
       $(".announcement_banner_content").slideUp(240);
