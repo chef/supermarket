96ba0c8035dd3e3496f25bdfc56342547051d27e
Logan McDonald <loganmcdona11@gmail.com>
2016-05-20 15:40:15 -0700

    adds extra expectation that previous removals of cookbook versions return 200 status, but last attempt on the final version throws a 409 status and a helpful error message

--- src/supermarket/spec/controllers/api/v1/cookbook_uploads_controller_spec.rb
+++ src/supermarket/spec/controllers/api/v1/cookbook_uploads_controller_spec.rb
@@ -250,8 +250,9 @@ describe Api::V1::CookbookUploadsController do
       it 'returns a 409 with informative error messages' do
         delete :destroy_version, cookbook: cookbook, version: '1.2.1', format: :json
         delete :destroy_version, cookbook: cookbook, version: '1.2.2', format: :json
-        delete :destroy_version, cookbook: cookbook, version: '1.2.3', format: :json
+        expect(response.status.to_i).to eql(200)
 
+        delete :destroy_version, cookbook: cookbook, version: '1.2.3', format: :json
         expect(response.status.to_i).to eql(409)
         expect(JSON.parse(response.body)["error_code"]).to eq('CONFLICT')
         expect(JSON.parse(response.body)["error_messages"][0]).to eq('You may not DELETE the only version of a cookbook. A cookbook must have at least one version.')
