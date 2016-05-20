9386ea876c8eaa1041e3e2c9b17571ad914a0912
Logan McDonald <loganmcdona11@gmail.com>
2016-05-20 15:58:54 -0700

    resolves error with dynamically changing cookbook version in factory by collecting versions and iterating over them to remove them

--- src/supermarket/spec/controllers/api/v1/cookbook_uploads_controller_spec.rb
+++ src/supermarket/spec/controllers/api/v1/cookbook_uploads_controller_spec.rb
@@ -248,11 +248,11 @@ describe Api::V1::CookbookUploadsController do
       before { auto_authorize!(Cookbook, 'destroy') }
 
       it 'returns a 409 with informative error messages' do
-        delete :destroy_version, cookbook: cookbook, version: '1.2.1', format: :json
-        delete :destroy_version, cookbook: cookbook, version: '1.2.2', format: :json
-        expect(response.status.to_i).to eql(200)
+        versions = CookbookVersion.all.pluck(:version)
+        versions.each do |version|
+          delete :destroy_version, cookbook: cookbook, version: version, format: :json
+        end
 
-        delete :destroy_version, cookbook: cookbook, version: '1.2.3', format: :json
         expect(response.status.to_i).to eql(409)
         expect(JSON.parse(response.body)["error_code"]).to eq('CONFLICT')
         expect(JSON.parse(response.body)["error_messages"][0]).to eq('You may not DELETE the only version of a cookbook. A cookbook must have at least one version.')
