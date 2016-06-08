eff34ab093ea11235128766d26e2b273d5c82728
Logan McDonald <loganmcdona11@gmail.com>
2016-06-08 15:37:21 -0700

    updated failing specs to include the confirmation warnings

--- src/supermarket/spec/helpers/adoption_helper_spec.rb
+++ src/supermarket/spec/helpers/adoption_helper_spec.rb
@@ -10,14 +10,14 @@ describe AdoptionHelper do
     it 'generates a link to enable adoption for a Cookbook' do
       cookbook = create(:cookbook, name: 'haha', up_for_adoption: false)
       link = helper.link_to_adoption(cookbook)
-      expected_link = '<li><a data-method="patch" href="/cookbooks/haha?cookbook%5Bup_for_adoption%5D=true" rel="nofollow"><i class="fa fa-heart"></i> Put up for adoption</a></li>'
+      expected_link = "<li><a data-confirm=\"Are you sure you want to put this up for adoption?\" data-method=\"patch\" href=\"/cookbooks/haha?cookbook%5Bup_for_adoption%5D=true\" rel=\"nofollow\"><i class=\"fa fa-heart\"></i> Put up for adoption</a></li>"
       expect(link).to eql(expected_link)
     end
 
     it 'generates a link to disable adoption for a Cookbook' do
       cookbook = create(:cookbook, name: 'haha', up_for_adoption: true)
       link = helper.link_to_adoption(cookbook)
-      expected_link = '<li><a data-method="patch" href="/cookbooks/haha?cookbook%5Bup_for_adoption%5D=false" rel="nofollow"><i class="fa fa-heart"></i> Disable adoption</a></li>'
+      expected_link = "<li><a data-confirm=\"Are you sure you want to put this up for adoption?\" data-method=\"patch\" href=\"/cookbooks/haha?cookbook%5Bup_for_adoption%5D=false\" rel=\"nofollow\"><i class=\"fa fa-heart\"></i> Disable adoption</a></li>"
       expect(link).to eql(expected_link)
     end
   end
@@ -26,14 +26,14 @@ describe AdoptionHelper do
     it 'generates a link to enable adoption for a Tool' do
       tool = create(:tool, name: 'haha', up_for_adoption: false)
       link = helper.link_to_adoption(tool)
-      expected_link = '<li><a data-method="patch" href="/tools/haha?tool%5Bup_for_adoption%5D=true" rel="nofollow"><i class="fa fa-heart"></i> Put up for adoption</a></li>'
+      expected_link = "<li><a data-confirm=\"Are you sure you want to put this up for adoption?\" data-method=\"patch\" href=\"/tools/haha?tool%5Bup_for_adoption%5D=true\" rel=\"nofollow\"><i class=\"fa fa-heart\"></i> Put up for adoption</a></li>"
       expect(link).to eql(expected_link)
     end
 
     it 'generates a link to disable adoption for a Tool' do
       tool = create(:tool, name: 'haha', up_for_adoption: true)
       link = helper.link_to_adoption(tool)
-      expected_link = '<li><a data-method="patch" href="/tools/haha?tool%5Bup_for_adoption%5D=false" rel="nofollow"><i class="fa fa-heart"></i> Disable adoption</a></li>'
+      expected_link = "<li><a data-confirm=\"Are you sure you want to put this up for adoption?\" data-method=\"patch\" href=\"/tools/haha?tool%5Bup_for_adoption%5D=false\" rel=\"nofollow\"><i class=\"fa fa-heart\"></i> Disable adoption</a></li>"
       expect(link).to eql(expected_link)
     end
   end
