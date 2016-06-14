e7635dc2d774178e00d511d1671946af64e11d85
Logan McDonald <loganmcdona11@gmail.com>
2016-06-14 11:01:13 -0700

    adds logic to pass fieri jobs request spec that increases the jobs size by 1

--- src/fieri/app/controllers/fieri/jobs_controller.rb
+++ src/fieri/app/controllers/fieri/jobs_controller.rb
@@ -3,7 +3,8 @@ require_dependency 'fieri/application_controller'
 module Fieri
   class JobsController < ApplicationController
     def create
-      FoodcriticWorker.perform_async(job_params)
+      FoodcriticWorker.perform_async(foodcritic_params)
+      CollaboratorWorker.perform_async(collaborator_params)
       render json: { status: 'ok' }.to_json
     rescue ActionController::ParameterMissing => e
       render status: 400, json: { status: 'error',
@@ -12,11 +13,15 @@ module Fieri
 
     private
 
-    def job_params
+    def foodcritic_params
       [:cookbook_name, :cookbook_version, :cookbook_artifact_url].each do |param|
         params.require(param)
       end
       params
+    end
+
+    def collaborator_params
+      params.require(:cookbook_name)
     end
   end
 end
--- src/fieri/app/models/collaborator_worker.rb
+++ src/fieri/app/models/collaborator_worker.rb
@@ -20,24 +20,5 @@ class CollaboratorWorker
   end
 
   def perform(params)
-    # begin
-    #   cookbook = CookbookArtifact.new(params['cookbook_artifact_url'], jid)
-    # rescue Zlib::GzipFile::Error => e
-    #   logger = Logger.new File.new(File.expand_path('./log/fieri.log'))
-    #   logger.error e
-    # else
-    #   feedback, status = cookbook.criticize
-
-    #   Net::HTTP.post_form(
-    #     URI.parse(ENV['FIERI_RESULTS_ENDPOINT']),
-    #     fieri_key: ENV['FIERI_KEY'],
-    #     cookbook_name: params['cookbook_name'],
-    #     cookbook_version: params['cookbook_version'],
-    #     collaborator_feedback: feedback,
-    #     collaborator_failure: status
-    #   )
-
-    #   cookbook.cleanup
-    # end
   end
 end
