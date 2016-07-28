1689a778508ab9d0eb2fa813f3a94392a6dbf222
Nell Shamrell <nellshamrell@gmail.com>
2016-07-28 11:01:49 -0700

    refactors host file in library to use ENV['FQDN']

--- src/supermarket/lib/supermarket/host.rb
+++ src/supermarket/lib/supermarket/host.rb
@@ -2,9 +2,9 @@ module Supermarket
   module Host
     def self.full_url
       if ENV['PORT'].present? && !%w(80 443).include?(ENV['PORT'])
-        "#{ENV['PROTOCOL']}://#{ENV['HOST']}:#{ENV['PORT']}"
+        "#{ENV['PROTOCOL']}://#{ENV['FQDN']}:#{ENV['PORT']}"
       else
-        "#{ENV['PROTOCOL']}://#{ENV['HOST']}"
+        "#{ENV['PROTOCOL']}://#{ENV['FQDN']}"
       end
     end
   end
