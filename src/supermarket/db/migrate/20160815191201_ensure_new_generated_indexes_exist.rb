7e07da0b64623ee514fbebdfecfcf9a8b49e82c4
Robb Kidd <rkidd@chef.io>
2016-08-16 14:03:17 -0400

    Fix index name too long in an old migration.
    Setting a specific name for the index keeps it from exceeding
    PG's 63 character limit. When the table is renamed later, the
    index is also renamed so that it follows Rails convention and
    subsequent modifications to the table will update the index.
    
    schema.rb updated by Rails to use the new force: :cascade
    default in SchemaDumper[1].
    
    > SchemaDumper uses force: :cascade on create_table. This
    > makes it possible to reload a schema when foreign keys are
    > in place.
    
    schema.rb also was updated by Rails 4.2 migrations to create
    new indexes not previously generated by the framework. These
    indexes are on columns have a uniqueness constraint, are
    used as a parameter in a scope, or are a reference to an id
    in another table. These are all good things, so a migration
    was also added to ensure these indexes get created in
    databases created by previous versions of Supermarket.
    
    [1] http://edgeguides.rubyonrails.org/4_2_release_notes.html#active-record-notable-changes
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

---
+++ src/supermarket/db/migrate/20160815191201_ensure_new_generated_indexes_exist.rb
@@ -0,0 +1,17 @@
+class EnsureNewGeneratedIndexesExist < ActiveRecord::Migration
+  def up
+    maybe_add_index "accounts", ["uid"], name: "index_accounts_on_uid", using: :btree
+    maybe_add_index "accounts", ["username"], name: "index_accounts_on_username", using: :btree
+    maybe_add_index "cclas", ["version"], name: "index_cclas_on_version", using: :btree
+    maybe_add_index "curry_pull_request_commit_authors", ["commit_author_id"], name: "index_curry_pull_request_commit_authors_on_commit_author_id", using: :btree
+    maybe_add_index "curry_pull_request_commit_authors", ["pull_request_id"], name: "index_curry_pull_request_commit_authors_on_pull_request_id", using: :btree
+    maybe_add_index "curry_pull_requests", ["repository_id"], name: "index_curry_pull_requests_on_repository_id", using: :btree
+    maybe_add_index "iclas", ["version"], name: "index_iclas_on_version", using: :btree
+  end
+
+  def maybe_add_index(table, columns, opts)
+    unless index_exists?(table, columns, opts)
+      add_index table, columns, opts
+    end
+  end
+end
