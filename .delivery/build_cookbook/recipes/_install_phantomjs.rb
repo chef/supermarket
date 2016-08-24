d9901f804158e64a9273762283d6eb173ce4f47a
Nell Shamrell <nellshamrell@gmail.com>
2016-08-24 14:48:27 -0700

    uses a chef controlled s3 bucket to download phantomjs

--- .delivery/build_cookbook/recipes/_install_phantomjs.rb
+++ .delivery/build_cookbook/recipes/_install_phantomjs.rb
@@ -3,8 +3,9 @@
 #########################################################################
 
 remote_file 'Retrieve a pre-built PhantomJS' do
-  source 'http://cnpmjs.org/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2'
+  source 'https://chef-releng.s3.amazonaws.com/phantomjs-2.1.1-linux-x86_64.tar.bz2'
   path "#{node['delivery']['workspace']['cache']}/phantomjs-2.1.1-linux-x86_64.tar.bz2"
+  checksum '86dd9a4bf4aee45f1a84c9f61cf1947c1d6dce9b9e8d2a907105da7852460d2f'
 end
 
 execute 'Place PhantomJS in path' do
