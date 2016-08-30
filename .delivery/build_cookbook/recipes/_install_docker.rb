529e6e7d2715224a844359787ff6ecdb0ab0275e
Robb Kidd <rkidd@chef.io>
2016-08-30 08:40:44 -0400

    Place docker-compose in project's workspace cache
    ... instead of potentially clobbering a docker-compose in /usr/local/bin
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- .delivery/build_cookbook/recipes/_install_docker.rb
+++ .delivery/build_cookbook/recipes/_install_docker.rb
@@ -5,7 +5,7 @@ include_recipe 'chef-apt-docker::default'
 
 package "docker-engine"
 
-remote_file '/usr/local/bin/docker-compose' do
+remote_file "#{delivery_workspace_cache}/docker-compose" do
   source 'https://github.com/docker/compose/releases/download/1.8.0/docker-compose-Linux-x86_64'
   checksum 'ebc6ab9ed9c971af7efec074cff7752593559496d0d5f7afb6bfd0e0310961ff'
   owner 'root'
