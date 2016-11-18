3d24916161c8c125764f746cfab0f55bc89b2a43
Robb Kidd <rkidd@chef.io>
2016-11-18 15:22:27 -0500

    set QM.admin_only boolean column to default to false, disallow NULL
    This should be a safe migration to modify because the previous version of
    the migration class file was never released. Only dev environments will
    need to fiddle with existing data, or `db:drop db:create db:migrate db:seed`
    to get the latest correct schema.
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- src/supermarket/app/models/metric_result.rb
+++ src/supermarket/app/models/metric_result.rb
@@ -2,7 +2,7 @@ class MetricResult < ActiveRecord::Base
   belongs_to :cookbook_version
   belongs_to :quality_metric
 
-  scope :open, -> { joins(:quality_metric).where('quality_metrics.admin_only IS NULL') }
+  scope :open, -> { joins(:quality_metric).where('quality_metrics.admin_only = false') }
   scope :admin_only, -> { joins(:quality_metric).where('quality_metrics.admin_only = true') }
 
   delegate :name, to: :quality_metric
--- src/supermarket/db/migrate/20161116191103_add_admin_only_to_quality_metrics.rb
+++ src/supermarket/db/migrate/20161116191103_add_admin_only_to_quality_metrics.rb
@@ -1,5 +1,5 @@
 class AddAdminOnlyToQualityMetrics < ActiveRecord::Migration
   def change
-    add_column :quality_metrics, :admin_only, :boolean
+    add_column :quality_metrics, :admin_only, :boolean, default: false, null: false
   end
 end
