272cfd1dcb5f492ade50a21112a6efd9fe2ad0fc
Nell Shamrell <nellshamrell@gmail.com>
2016-11-28 15:14:20 -0800

    adds code and specs to handle when a cookbook version does not pass the license metric
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/fieri/app/models/license_worker.rb
+++ src/fieri/app/models/license_worker.rb
@@ -5,16 +5,23 @@ class LicenseWorker
   include ::Sidekiq::Worker
 
   def perform(version_json, cookbook_name)
-    failure = false
-    license_feedback = ''
-
     Net::HTTP.post_form(
       URI.parse("#{ENV['FIERI_SUPERMARKET_ENDPOINT']}/api/v1/cookbook-versions/license_evaluation"),
       fieri_key: ENV['FIERI_KEY'],
       cookbook_name: cookbook_name,
       cookbook_version: JSON.parse(version_json)['version'],
-      license_failure: failure,
-      license_feedback: license_feedback
+      license_failure: license_failure?(version_json),
+      license_feedback: license_feedback(cookbook_name)
     )
+  end
+
+  private
+
+  def license_failure?(version_json)
+    JSON.parse(version_json)['license'].present? ? false : true
+  end
+
+  def license_feedback(cookbook_name)
+    "#{cookbook_name} has no license"
   end
 end
--- src/fieri/spec/models/license_worker_spec.rb
+++ src/fieri/spec/models/license_worker_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 describe LicenseWorker do
+  let(:cookbook_name) { 'apt' }
+  let(:lw) { LicenseWorker.new }
+
+  before do
+    stub_request(:post, "#{ENV['FIERI_SUPERMARKET_ENDPOINT']}/api/v1/cookbook-versions/license_evaluation").
+      to_return(status: 200, body: '', headers: {})
+  end
+
   context 'when the cookbook version has a license' do
     let(:licensed_version_json_response) { File.read('spec/support/cookbook_version_fixture.json') }
-    let(:cookbook_name) { 'apt' }
     let(:cookbook_version) { JSON.parse(licensed_version_json_response)['version'] }
-    let(:lw) { LicenseWorker.new }
 
     before do
       stub_request(:post, "#{ENV['FIERI_SUPERMARKET_ENDPOINT']}/api/v1/cookbook-versions/license_evaluation").
@@ -25,7 +31,23 @@ describe LicenseWorker do
   end
 
   context 'when the cookbook version does not have a license' do
-    it 'indicates that the license metric failed'
-    it 'includes a message in the feedback'
+    let(:no_license_version_json_response) { File.read('spec/support/cookbook_version_no_license_fixture.json') }
+    let(:cookbook_version) { JSON.parse(no_license_version_json_response)['version'] }
+
+    before do
+      stub_request(:post, "#{ENV['FIERI_SUPERMARKET_ENDPOINT']}/api/v1/cookbook-versions/license_evaluation").
+        to_return(status: 200, body: '', headers: {})
+    end
+
+    it 'indicates that the license metric failed' do
+      lw.perform(no_license_version_json_response, cookbook_name)
+
+      assert_requested(:post, "#{ENV['FIERI_SUPERMARKET_ENDPOINT']}/api/v1/cookbook-versions/license_evaluation", times: 1) do |req|
+         expect(req.body).to include("cookbook_name=#{cookbook_name}")
+         expect(req.body).to include("cookbook_version=#{cookbook_version}")
+         expect(req.body).to include('license_failure=true')
+         expect(req.body).to include("license_feedback=#{cookbook_name}+has+no+license")
+      end
+    end
   end
 end
