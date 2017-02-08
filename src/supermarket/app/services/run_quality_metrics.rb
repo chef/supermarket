31c3c5327ea44d4661a32c8844323c38db0d83f5
Robb Kidd <rkidd@chef.io>
2017-02-08 10:04:11 -0500

    display error when runs for non-existent cookbooks are requested
    Changed the return of the service class methods to be success/failure
    tuples for consumers of service to deal with errors without exception
    handling. (i.e. bad user input is not exceptional)
    
    Added guards for when metric runs are requested and fieri is disabled.
    
    Added errors for missing cookbooks or cookbook versions for the methods
    that take potentially mistaken user input.
    
    Internationalize, yo!
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- src/supermarket/app/services/run_quality_metrics.rb
+++ src/supermarket/app/services/run_quality_metrics.rb
@@ -1,25 +1,45 @@
 class RunQualityMetrics
   def self.all_the_latest
-    CookbookVersion.where('
+    return [:error, I18n.t('fieri.not_enabled')] unless ROLLOUT.active? :fieri
+
+    cookbook_version_ids = CookbookVersion.where('
       cookbook_versions.created_at = (
         SELECT MAX(latest.created_at)
         FROM cookbook_versions AS latest
         WHERE latest.cookbook_id = cookbook_versions.cookbook_id
       )
-    ').pluck(:id).each do |cookbook_version_id|
+    ').pluck(:id)
+
+    cookbook_version_ids.each do |cookbook_version_id|
       FieriNotifyWorker.perform_async cookbook_version_id
     end
+
+    [:ok, I18n.t('fieri.scheduled.multiple', count: cookbook_version_ids.length)]
   end
 
   def self.on_latest(cookbook_name)
+    return [:error, I18n.t('fieri.not_enabled')] unless ROLLOUT.active? :fieri
+
     cookbook = Cookbook.find_by_name cookbook_name
+    return [:error, I18n.t('cookbook.not_found', name: cookbook_name)] unless cookbook
+
     latest_cookbook_version = cookbook.latest_cookbook_version
     FieriNotifyWorker.perform_async latest_cookbook_version.id
+
+    [:ok, I18n.t('fieri.scheduled.single', name: cookbook.name, version: latest_cookbook_version.version)]
   end
 
   def self.on_version(cookbook_name, version)
+    return [:error, I18n.t('fieri.not_enabled')] unless ROLLOUT.active? :fieri
+
     cookbook = Cookbook.find_by_name cookbook_name
+    return [:error, I18n.t('cookbook.not_found', name: cookbook_name)] unless cookbook
+
     cookbook_version = cookbook.cookbook_versions.find_by_version version
+    return [:error, I18n.t('cookbook.version_not_found', name: cookbook_name, version: version)] unless cookbook_version
+
     FieriNotifyWorker.perform_async cookbook_version.id
+
+    [:ok, I18n.t('fieri.scheduled.single', name: cookbook.name, version: cookbook_version.version)]
   end
 end
--- src/supermarket/spec/services/run_quality_metrics_spec.rb
+++ src/supermarket/spec/services/run_quality_metrics_spec.rb
@@ -1,30 +1,114 @@
 require 'spec_helper'
 
 describe RunQualityMetrics do
-  context 'schedules quality metric runs for' do
-    it 'the latest version of a single cookbook' do
-      cookbook = create(:cookbook)
-      latest_version = cookbook.latest_cookbook_version
-
-      expect(FieriNotifyWorker).to receive(:perform_async).with(latest_version.id)
-      RunQualityMetrics.on_latest(cookbook.name)
+  context 'when fieri is enabled' do
+    before :each do
+      expect(ROLLOUT).to receive(:active?).with(:fieri).and_return(true)
     end
 
-    it 'a given version of a single cookbook' do
-      cookbook = create(:cookbook)
-      cookbook_version = cookbook.cookbook_versions.first
+    context 'on the latest version of a single cookbook' do
+      let(:cookbook) { create :cookbook }
+      let(:latest_version) { cookbook.latest_cookbook_version }
 
-      expect(FieriNotifyWorker).to receive(:perform_async).with(cookbook_version.id)
-      RunQualityMetrics.on_version(cookbook.name, cookbook_version.version)
-    end
-
-    it 'all the latest cookbook versions' do
-      13.times do
-        create(:cookbook)
+      it 'schedules notifying fieri to do the thing' do
+        expect(FieriNotifyWorker).to receive(:perform_async).with(latest_version.id)
+        RunQualityMetrics.on_latest(cookbook.name)
       end
 
-      expect(FieriNotifyWorker).to receive(:perform_async).exactly(13).times
-      RunQualityMetrics.all_the_latest
+      it 'returns a message about successful scheduling' do
+        allow(FieriNotifyWorker).to receive(:perform_async)
+        success_message = I18n.t('fieri.scheduled.single', name: cookbook.name, version: latest_version.version)
+
+        expect(RunQualityMetrics.on_latest(cookbook.name))
+          .to eql([:ok, success_message])
+      end
+
+      it 'returns an error when a cookbook is not found with a given name' do
+        expect(FieriNotifyWorker).not_to receive(:perform_async)
+        error_message = I18n.t('cookbook.not_found', name: 'nope')
+
+        expect(RunQualityMetrics.on_latest('nope'))
+          .to eql([:error, error_message])
+      end
+    end
+
+    context 'on a given version of a single cookbook' do
+      let(:cookbook) { create :cookbook }
+      let(:cookbook_version) { cookbook.cookbook_versions.first }
+
+      it 'schedules notifying fieri to do the thing' do
+        expect(FieriNotifyWorker).to receive(:perform_async).with(cookbook_version.id)
+        RunQualityMetrics.on_version(cookbook.name, cookbook_version.version)
+      end
+
+      it 'returns a message about successful scheduling' do
+        allow(FieriNotifyWorker).to receive(:perform_async)
+        success_message = I18n.t('fieri.scheduled.single', name: cookbook.name, version: cookbook_version.version)
+
+        expect(RunQualityMetrics.on_version(cookbook.name, cookbook_version.version))
+          .to eql([:ok, success_message])
+      end
+
+      it 'returns an error when a cookbook is not found with a given name' do
+        expect(FieriNotifyWorker).not_to receive(:perform_async)
+        error_message = I18n.t('cookbook.not_found', name: 'nope')
+
+        expect(RunQualityMetrics.on_version('nope', '9.9.9'))
+          .to eql([:error, error_message])
+      end
+
+      it 'returns an error when a cookbook is not found with a given version' do
+        cookbook = create(:cookbook, name: 'got-no-nines')
+        expect(FieriNotifyWorker).not_to receive(:perform_async)
+        error_message = I18n.t('cookbook.version_not_found', name: 'got-no-nines', version: '9.9.9')
+
+        expect(RunQualityMetrics.on_version('got-no-nines', '9.9.9'))
+          .to eql([:error, error_message])
+      end
+    end
+
+    context 'on all the latest cookbook versions' do
+      before :each do
+        13.times do
+          create(:cookbook)
+        end
+      end
+
+      it 'schedules notifying fieri to do the thing' do
+        expect(FieriNotifyWorker).to receive(:perform_async).exactly(13).times
+        RunQualityMetrics.all_the_latest
+      end
+
+      it 'returns a message about successful scheduling' do
+        allow(FieriNotifyWorker).to receive(:perform_async)
+        success_message = I18n.t('fieri.scheduled.multiple', count: 13)
+
+        expect(RunQualityMetrics.all_the_latest)
+          .to eq([:ok, success_message])
+      end
+    end
+  end
+
+  context 'when fieri is not enabled' do
+    before :each do
+      expect(ROLLOUT).to receive(:active?).with(:fieri).and_return(false)
+    end
+
+    let(:error_message) { I18n.t('fieri.not_enabled') }
+
+    it 'shows a disabled notice for all the latest' do
+      expect(RunQualityMetrics.all_the_latest)
+        .to eql([:error, error_message])
+    end
+
+    it 'shows a disabled notice for one cookbook latest' do
+      expect(RunQualityMetrics.on_latest('nope'))
+        .to eql([:error, error_message])
+    end
+
+    it 'shows a disabled notice for one cookbook version' do
+      expect(RunQualityMetrics.on_version('nope', '1.2.3'))
+        .to eql([:error, error_message])
     end
   end
 end
