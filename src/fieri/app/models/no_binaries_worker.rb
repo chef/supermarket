e1dd45ab5c167416862a63376dcf4fe6aafee113
Robb Kidd <rkidd@chef.io>
2017-03-17 08:32:19 -0400

    add no_binaries worker
    Worker modeled after foodcritic worker to check for binaries.
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

---
+++ src/fieri/app/models/no_binaries_worker.rb
@@ -0,0 +1,40 @@
+require 'sidekiq'
+require 'net/http'
+
+class NoBinariesWorker
+  include ::Sidekiq::Worker
+
+  def perform(params)
+    cookbook = CookbookArtifact.new(params['cookbook_artifact_url'], jid)
+    binary_files = cookbook.binaries
+
+    failure = !binary_files.empty?
+    feedback = if failure
+                 I18n.t('quality_metrics.no_binaries.failure') + "\n" + binary_files
+               else
+                 I18n.t('quality_metrics.no_binaries.success')
+               end
+
+    make_post(params, feedback, failure)
+    cookbook.cleanup
+  rescue => e
+    log_error(e)
+  end
+
+  def log_error(e)
+    logger.error e
+  end
+
+  def make_post(params, feedback, failure)
+    response = Net::HTTP.post_form(
+      URI.parse("#{ENV['FIERI_SUPERMARKET_ENDPOINT']}/api/v1/quality_metrics/no_binaries_evaluation"),
+      fieri_key: ENV['FIERI_KEY'],
+      cookbook_name: params['cookbook_name'],
+      cookbook_version: params['cookbook_version'],
+      no_binaries_feedback: feedback,
+      no_binaries_failure: failure
+    )
+    return if response.is_a? Net::HTTPSuccess
+    raise "Unable to POST No Binaries Evaluation of #{params['cookbook_name']} " + response.message
+  end
+end
