25a82f9a17a02cf4541850afb8345fac600181d4
Robb Kidd <rkidd@chef.io>
2017-04-19 11:32:18 -0400

    make metrics display in a consistent order
    For now, we'll go alphabetical by name.
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- src/supermarket/app/models/metric_result.rb
+++ src/supermarket/app/models/metric_result.rb
@@ -4,6 +4,7 @@ class MetricResult < ActiveRecord::Base
 
   scope :open, -> { joins(:quality_metric).merge(QualityMetric.open) }
   scope :admin_only, -> { joins(:quality_metric).merge(QualityMetric.admin_only) }
+  scope :sorted_by_name, -> { joins(:quality_metric).order('quality_metrics.name asc') }
 
   delegate :name, to: :quality_metric
   delegate :admin_only?, to: :quality_metric
