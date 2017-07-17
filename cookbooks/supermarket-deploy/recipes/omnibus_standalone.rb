fda752bad1c3edff28a865ee556d62dbb9378869
Tom Duffield <tom@chef.io>
2017-07-17 14:52:46 -0500

    Use wildcard SSL cert for ACC environments
    Signed-off-by: Tom Duffield <tom@chef.io>

--- cookbooks/supermarket-deploy/recipes/omnibus_standalone.rb
+++ cookbooks/supermarket-deploy/recipes/omnibus_standalone.rb
@@ -6,6 +6,31 @@
 
 include_recipe 'supermarket-deploy::default'
 
+################################################################
+# Download SSL Certs from Citadel
+################################################################
+
+directory "/var/opt/supermarket/ssl/ca" do
+  mode "0755"
+  recursive true
+end
+
+file "/var/opt/supermarket/ssl/ca/wildcard.chef.co.crt" do
+  mode "0600"
+  content citadel['wildcard.chef.co.crt']
+  sensitive true
+end
+
+file "/var/opt/supermarket/ssl/ca/wildcard.chef.co.key" do
+  mode "0600"
+  content citadel['wildcard.chef.co.key']
+  sensitive true
+end
+
+################################################################
+# Install Supermarket
+################################################################
+
 supermarket_ocid = Chef::JSONCompat.from_json(Chef::HTTP.new("https://#{server_fqdn_for('chef-server')}").get('/supermarket-credentials'))
 
 chef_ingredient 'supermarket' do
@@ -16,7 +41,11 @@ chef_ingredient 'supermarket' do
     host: server_fqdn_for('supermarket'),
     chef_server_url: "https://#{server_fqdn_for('chef-server')}",
     chef_oauth2_app_id: supermarket_ocid['uid'],
-    chef_oauth2_secret: supermarket_ocid['secret']
+    chef_oauth2_secret: supermarket_ocid['secret'],
+    ssl: {
+      certificate: "/var/opt/supermarket/ssl/ca/wildcard.chef.co.crt",
+      certificate_key: "/var/opt/supermarket/ssl/ca/wildcard.chef.co.key",
+    }
   )
 
   action :upgrade
