fb11f695e3edc8fc6c3f15af5682d80fbbbcfb3c
Robb Kidd <rkidd@chef.io>
2018-01-22 15:10:03 -0500

    disable remote fetching in Paperclip
    Upgrade to Paperclip 5.2+. There is a CVE[1] in Paperclip v5.1.0.[1]
    
    [1] https://nvd.nist.gov/vuln/detail/CVE-2017-0889
    
    ***** Supermarket was not vulnerable to this CVE. *****
    
    The `tarball` parameter given for an upload is validated by
    CookbookUpload::Parameters and CookbookUpload::Archive before a hand-off
    to ActiveRecord+Paperclip for processing as an attachment.
    
    This update to the Paperclip initializer is intended to prevent future
    accidental support for remote fetching if the logic of CookbookUpload
    changes at some point and allows a URL string through to the persistence
    layer. Paperclip 5.2.0 disabled these remote adapters. Leaving the
    initializer disabling in case that behavior changes.
    
    Added tests to catch regressions.
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- src/supermarket/config/initializers/paperclip.rb
+++ src/supermarket/config/initializers/paperclip.rb
@@ -2,6 +2,15 @@ Paperclip.interpolates(:compatible_id) do |attachment, _style|
   attachment.instance.try(:legacy_id) || attachment.instance.id
 end
 
+# Paperclip has a few remote-fetcher adapters for retrieving attachments from some other place.
+# We don't want to use them. Remove these adapters, if they are present in the adapter registry.
+remote_adapters = [Paperclip::UriAdapter, Paperclip::HttpUrlProxyAdapter, Paperclip::DataUriAdapter]
+Paperclip.io_adapters
+         .registered_handlers
+         .delete_if do |_block, handler_class|
+           remote_adapters.include?(handler_class)
+         end
+
 ':class/:attachment/:compatible_id/:style/:basename.:extension'.tap do |path|
   if Supermarket::S3ConfigAudit.use_s3?(ENV)
     if ENV['S3_PATH'].present?
