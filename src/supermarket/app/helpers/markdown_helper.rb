e6eddf87b9bfde2eb027a7b980113525dac70206
Robb Kidd <rkidd@chef.io>
2018-07-17 16:05:29 -0400

    set links from markdown to open in new browser windows
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- src/supermarket/app/helpers/markdown_helper.rb
+++ src/supermarket/app/helpers/markdown_helper.rb
@@ -20,10 +20,41 @@ module MarkdownHelper
       HTML::Pipeline::ImageMaxWidthFilter,
       HTML::Pipeline::TableOfContentsFilter,
       HTML::Pipeline::HttpsFilter,
+      LinksTargetNewWindowFilter,
+      ImgProtocolRelativeFilter,
       HTML::Pipeline::MentionFilter
     ], context.merge(gfm: true)
 
     result = pipeline.call(text)
     result[:output].to_s.html_safe
+  end
+
+  class LinksTargetNewWindowFilter < HTML::Pipeline::HttpsFilter
+    #
+    # Sets every link in a document to open in a new browser window or tab
+    #
+    def call
+      doc.search("a").each do |link|
+        link["target"] = "_blank"
+      end
+      doc
+    end
+  end
+
+  class ImgProtocolRelativeFilter < HTML::Pipeline::HttpsFilter
+    #
+    # Converts the scheme of every img tag http/https src attribute
+    # to be a relative protocol
+    #
+    def call
+      doc.search("img").each do |img|
+        next if img['src'].nil?
+        src = img['src'].strip
+        if src.start_with? 'http'
+          img["src"] = src.sub(/\Ahttps?:/, '')
+        end
+      end
+      doc
+    end
   end
 end
