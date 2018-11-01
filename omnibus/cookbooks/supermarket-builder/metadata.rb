8ca3eb136aa32d52158a4077feff603ce16942da
Tim Smith <tsmith@chef.io>
2018-11-01 13:31:19 -0700

    Use the built-in apt resource in the omnibus build cookbook
    Modern Chef gives us apt_update out of the box. No need for the apt cookbook now.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus/cookbooks/supermarket-builder/metadata.rb
+++ omnibus/cookbooks/supermarket-builder/metadata.rb
@@ -3,9 +3,9 @@ maintainer       'Chef Supermarket Team'
 maintainer_email 'supermarket@chef.io'
 license          'Apache-2.0'
 description      'Builds a Supermarket omnibus package for development/testing'
-long_description 'Builds a Supermarket omnibus package for development/testing'
 version          '1.0.0'
 
-depends          'apt'
 depends          'yum-epel'
 depends          'omnibus'
+
+chef_version     '>= 13.0'
