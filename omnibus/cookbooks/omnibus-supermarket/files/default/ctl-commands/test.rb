efe136544b5c22a7c8dc5306b0f74bc2c2070242
Robb Kidd <rkidd@chef.io>
2019-08-16 14:46:34 -0400

    install inspec as a gem for the -ctl command
    The inspec software definition in omnibus-software does not (yet?)
    also install the inspec-bin gem to get binstub executables created.
    Because the supermarket-ctl command is the thing that needs inspec as a
    command to shell out to _and_ it is managed as a software definition
    _and_ the software definition is already bundle-installing, I've added
    inspec and inspec-bin to the Gemfile used by the software definition.
    
    This results in the latest _release_ inspec, as opposed to whatever is
    current on the master branch, getting installed within Supermarket as an
    executable. Licensing is handled at the call site in the -ctl command
    that uses inspec.
    
    Since we're bring in the latest, we need to accept the license for the
    duration of the -ctl test.
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- omnibus/cookbooks/omnibus-supermarket/files/default/ctl-commands/test.rb
+++ omnibus/cookbooks/omnibus-supermarket/files/default/ctl-commands/test.rb
@@ -16,6 +16,7 @@ add_command 'test', 'Run the Supermarket installation test suite', 2 do
   command_text += ' /opt/supermarket/embedded/cookbooks/omnibus-supermarket/test/integration/default/inspec'
   command_text += ' --color'
   command_text += ' --no-distinct-exit' # skipped tests are OK
+  command_text += ' --chef-license=accept-no-persist'
 
   if options[:junit_xml]
     command_text += " --reporter junit:#{options[:junit_xml]}"
