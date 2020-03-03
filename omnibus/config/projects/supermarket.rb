16e5d997a9944dba0a50d572d24b13f8a5e26b8d
Robb Kidd <rkidd@chef.io>
2020-03-03 17:25:22 -0500

    bump Ruby to 2.6.5, update gems with deprecations
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- omnibus/config/projects/supermarket.rb
+++ omnibus/config/projects/supermarket.rb
@@ -29,7 +29,7 @@ build_version   IO.read(File.expand_path("../../../../VERSION", __FILE__)).strip
 build_iteration 1
 
 override :postgresql, version: '9.3.18'
-override :ruby, version: "2.5.3"
+override :ruby, version: "2.6.5"
 override :rubygems, version: "3.0.3" # rubygems ships its own bundler which may differ from bundler defined below and then we get double bundler which makes the omnibus environment unhappy. Make sure these versions match before bumping either.
 override :bundler, version: "1.17.3" # this must match the BUNDLED WITH in all the repo's Gemfile.locks
 override :'chef-gem', version: '14.5.33'
