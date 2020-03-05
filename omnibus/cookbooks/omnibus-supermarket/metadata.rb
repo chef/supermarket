30f01699c2e865f0127e561c8fea89983d8f0282
Robb Kidd <rkidd@chef.io>
2020-03-05 12:18:49 -0500

    appease Cookstyle: ChefRedundantCode/LongDescriptionMetadata
    > The long_description metadata.rb method is not used and is unnecessary
    > in cookbooks.
    
    Signed-off-by: Robb Kidd <rkidd@chef.io>

--- omnibus/cookbooks/omnibus-supermarket/metadata.rb
+++ omnibus/cookbooks/omnibus-supermarket/metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Supermarket Team'
 maintainer_email 'supermarket@chef.io'
 license          'Apache-2.0'
 description      'Installs/Configures supermarket in an Omnibus installation'
-long_description 'Installs/Configures supermarket in an Omnibus installation'
 version          '1.1.1'
 
 chef_version     '>= 14.0'
