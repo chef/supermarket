95b0acf4642098d9bbc2f9af682026fa15f41e31
saghoshprogress <saghosh@progress.com>
2021-07-28 16:43:51 +0530

    Modified

--- omnibus/config/projects/supermarket.rb
+++ omnibus/config/projects/supermarket.rb
@@ -31,7 +31,7 @@ build_iteration 1
 override :postgresql, version: '9.3.25'
 override :ruby, version: "2.7.4"
 override :rubygems, version: "3.1.2" # rubygems ships its own bundler which may differ from bundler defined below and then we get double bundler which makes the omnibus environment unhappy. Make sure these versions match before bumping either.
-override :bundler, version: "2.1.4" # this must match the BUNDLED WITH in all the repo's Gemfile.locks
+override :bundler, version: "2.1.2" # this must match the BUNDLED WITH in all the repo's Gemfile.locks
 override :'chef-gem', version: '14.14.29'
 override :'openssl-fips', version: '2.0.16'
 override :'omnibus-ctl', version: 'master'
