2a5a46fc78c068d5b11959021f01968afc74d310
Rajesh Paul <rajesh.paul@progress.com>
2021-08-10 18:54:47 +0530

    changed cookie serializer from :hybrid to :json as hybrid serializer has security issues
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- src/supermarket/config/initializers/cookies_serializer.rb
+++ src/supermarket/config/initializers/cookies_serializer.rb
@@ -2,4 +2,10 @@
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
-Rails.application.config.action_dispatch.cookies_serializer = :hybrid
+
+# NOTE - Changed the serializer from :hybrid to :json as the rails
+# has been upgraded to version:5 around 4 years back.
+# Changing this now as the chances of preexisting cookies which are more than 4 years old is very less.
+# In worst case the cookie will not be supported and the user will be asked to signin again.
+# If that's an issue we will revert it back to :hybrid
+Rails.application.config.action_dispatch.cookies_serializer = :json
