afc0f2976d5ac8f2a2b8581ab60c219bb62b2da1
Rajesh Paul <rajesh.paul@progress.com>
2021-11-15 23:13:45 +0530

    Added class documentation
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- src/supermarket/app/models/cookbook_upload/document.rb
+++ src/supermarket/app/models/cookbook_upload/document.rb
@@ -1,5 +1,16 @@
 class CookbookUpload
   class Document < SchemaDefiner::SymbolizeStruct
+    
+    #
+    # @!attribute [r] contents
+    #   @return [String] The text of the Document
+    #
+
+    #
+    # @!attribute [r] extension
+    #   @return [String] The README extension
+    #
+
     attribute :contents, SchemaDefiner::Types::Coercible::String.default(nil)
     attribute :extension, SchemaDefiner::Types::Coercible::String.default("", shared: true)
   end
