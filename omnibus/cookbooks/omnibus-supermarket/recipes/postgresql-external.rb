0e34390e12bf3b2b2c5fe6ebc6a0e42111510066
RajeshPaul38 <85236476+RajeshPaul38@users.noreply.github.com>
2021-11-19 10:58:03 +0530

    review feedback
    Co-authored-by: Tim Smith <tsmith@chef.io>

--- omnibus/cookbooks/omnibus-supermarket/recipes/postgresql-external.rb
+++ omnibus/cookbooks/omnibus-supermarket/recipes/postgresql-external.rb
@@ -2,7 +2,7 @@
 # Cookbook:: supermarket
 # Recipe:: postgresql
 #
-# Copyright:: 2014 Chef Software, Inc.
+# Copyright:: Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
