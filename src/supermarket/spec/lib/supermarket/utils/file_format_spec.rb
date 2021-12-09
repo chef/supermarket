23dbe7410ae6020109d90ccf8c0ea34dce99f77d
Rajesh Paul <rajesh.paul@progress.com>
2021-12-09 16:30:38 +0530

    added testcase for Utils::FileFormat class
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

---
+++ src/supermarket/spec/lib/supermarket/utils/file_format_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Utils::FileFormat do
+  describe "Utils::FileFormat" do
+    let(:filepath) {"spec/assets/sample.txt.gz"}
+    let(:mime_type) {"application/gzip"}
+    it "returns gzip for gz file" do
+      expect(Utils::FileFormat.get_mime_type(file_path: filepath)).to eq(mime_type)
+    end
+  end
+end
