df1d1a3950e5a42356365ede3da4bffa23ffe76c
antima-gupta <agupta@msystechnologies.com>
2021-12-09 15:58:16 +0530

    Refactor the routes for universe.
    Signed-off-by: antima-gupta <agupta@msystechnologies.com>

--- src/supermarket/config/routes.rb
+++ src/supermarket/config/routes.rb
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
       delete "cookbooks/:cookbook" => "cookbook_uploads#destroy"
       delete "cookbooks/:cookbook/versions/:version" => "cookbook_uploads#destroy_version", constraints: { version: VERSION_PATTERN }
       get "users/:user" => "users#show", as: :user
-      get "universe" => "universe#index"
 
       # This was the original route, which has a misspelling (cookbook-verisons rather than cookbook-versions).  Keeping this here so as not to break anyone who is still depending on the original route.
       post "/cookbook-verisons/foodcritic_evaluation" => "quality_metrics#foodcritic_evaluation", constraints: proc { Feature.active?(:fieri) }
@@ -49,7 +48,11 @@ Rails.application.routes.draw do
 
   get "cookbooks-directory" => "cookbooks#directory"
   get "available_for_adoption" => "cookbooks#available_for_adoption"
-  get "universe" => "api/v1/universe#index", defaults: { format: :json }
+
+  ["universe", "api/v1/universe"].each do |universe_path|
+    get universe_path, to: "api/v1/universe#index", defaults: { format: :json }
+  end
+
   get "status" => "api/v1/health#show", defaults: { format: :json }
   get "unsubscribe/:token" => "email_preferences#unsubscribe", as: :unsubscribe
 
