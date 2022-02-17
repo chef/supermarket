bfaef78100b8253c7c429d34924e910ac85bb6a7
Rajesh Paul <rajesh.paul@progress.com>
2022-02-17 18:13:14 +0530

    passing template_path in it_behave_like for shared example
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- src/supermarket/spec/support/shared_examples/community_stats.rb
+++ src/supermarket/spec/support/shared_examples/community_stats.rb
@@ -1,8 +1,9 @@
-shared_examples "community stats" do
+# shared_examples "community stats" do
+shared_examples_for "community stats" do |template|
   it "displays cookbooks and chefs as singular if there is only 1" do
     assign(:cookbook_count, 1)
     assign(:user_count, 1)
-    render
+    render template: template
     expect(rendered).to match(%r{1 Cookbook</span>})
     expect(rendered).to match(%r{1 Chef</span>})
   end
@@ -10,7 +11,7 @@ shared_examples "community stats" do
   it "displays cookbooks and chefs as plural if there is more than 1" do
     assign(:cookbook_count, 2)
     assign(:user_count, 2)
-    render
+    render template: template
     expect(rendered).to match(%r{2 Cookbooks</span>})
     expect(rendered).to match(%r{2 Chefs</span>})
   end
@@ -18,7 +19,7 @@ shared_examples "community stats" do
   it "delimits numbers correctly if there are more than 999" do
     assign(:cookbook_count, 1000)
     assign(:user_count, 1000)
-    render
+    render template: template
     expect(rendered).to match(%r{1,000 Cookbooks</span>})
     expect(rendered).to match(%r{1,000 Chefs</span>})
   end
--- src/supermarket/spec/views/pages/welcome.html.erb_spec.rb
+++ src/supermarket/spec/views/pages/welcome.html.erb_spec.rb
@@ -1,11 +1,13 @@
 require "spec_helper"
 
 describe "pages/welcome.html.erb" do
+  let(:template_path) { "pages/welcome" }
+
   it "has correct text" do
-    render template: "pages/welcome"
+    render template: template_path
     welcome_text = "Welcome to Supermarket. Find, explore and view Chef Infra Cookbooks for all of your ops needs."
     expect(rendered).to have_selector("h2", text: welcome_text)
   end
 
-  it_behaves_like "community stats"
+  it_behaves_like "community stats", "pages/welcome"
 end
