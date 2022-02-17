f8dd80e84aad847eee37acab88c64261838151f6
Rajesh Paul <rajesh.paul@progress.com>
2022-02-17 16:47:55 +0530

    lint errors fix
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- src/supermarket/config/initializers/psych_initializer.rb
+++ src/supermarket/config/initializers/psych_initializer.rb
@@ -1,10 +1,10 @@
-Psych::ClassLoader::ALLOWED_PSYCH_CLASSES = [ Symbol, Time ]
+Psych::ClassLoader::ALLOWED_PSYCH_CLASSES = [ Symbol, Time ].freeze
 
 module Psych
   class ClassLoader
-    ALLOWED_PSYCH_CLASSES = [] unless defined? ALLOWED_PSYCH_CLASSES
+    ALLOWED_PSYCH_CLASSES = [].freeze unless defined? ALLOWED_PSYCH_CLASSES
     class Restricted < ClassLoader
-      def initialize classes, symbols
+      def initialize(classes, symbols)
         @classes = classes + Psych::ClassLoader::ALLOWED_PSYCH_CLASSES.map(&:to_s)
         @symbols = symbols
         super()
