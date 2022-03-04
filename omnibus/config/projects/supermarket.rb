56a2eeda63f6a595a68aca7055faf090153f17d0
Rajesh Paul <rajesh.paul@progress.com>
2022-03-04 16:48:34 +0530

    using nodejs instead of nodejs-binary as omnibus dependency
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- omnibus/config/projects/supermarket.rb
+++ omnibus/config/projects/supermarket.rb
@@ -35,7 +35,7 @@ override :'openssl-fips', version: '2.0.16'
 override :'omnibus-ctl', version: 'main'
 override :openssl, version: '1.0.2za'
 override :logrotate, version: '3.9.2'
-override 'nodejs-binary', version: '6.7.0'
+override :nodejs, version: '12.22.3'
 
 # Creates required build directories
 dependency "preparation"
