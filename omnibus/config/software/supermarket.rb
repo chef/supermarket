97ee8ab52166d412644559b1d96c5ce719e35e02
Rajesh Paul <rajesh.paul@progress.com>
2022-03-09 13:02:46 +0530

    added nodejs-binary back
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- omnibus/config/software/supermarket.rb
+++ omnibus/config/software/supermarket.rb
@@ -29,6 +29,7 @@ dependency "runit"
 dependency "logrotate"
 dependency "file"
 dependency "libarchive"
+dependency "nodejs-binary"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
