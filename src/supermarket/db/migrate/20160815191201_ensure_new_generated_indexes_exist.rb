be9451dbeb2ed16d05c6e68aaee61ceac203ff31
Rajesh Paul <rajesh.paul@progress.com>
2022-03-09 22:06:47 +0530

    wrong number of arguments for index_exists? - fix
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- src/supermarket/db/migrate/20160815191201_ensure_new_generated_indexes_exist.rb
+++ src/supermarket/db/migrate/20160815191201_ensure_new_generated_indexes_exist.rb
@@ -1,7 +1,7 @@
 class EnsureNewGeneratedIndexesExist < ActiveRecord::Migration[4.2]
   def up
-    maybe_add_index "accounts", ["uid"], { name: "index_accounts_on_uid", using: :btree }
-    maybe_add_index "accounts", ["username"], { name: "index_accounts_on_username", using: :btree }
+    maybe_add_index "accounts", "uid", { name: "index_accounts_on_uid", using: :btree }
+    maybe_add_index "accounts", "username", { name: "index_accounts_on_username", using: :btree }
   end
 
   def maybe_add_index(table, columns, opts)
