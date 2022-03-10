e6c08135f12739cd65fa5d25b240a7cb98bb4f20
Rajesh Paul <rajesh.paul@progress.com>
2022-03-10 12:21:37 +0530

    changed add_index statement to make it compatible with rails 7
    Signed-off-by: Rajesh Paul <rajesh.paul@progress.com>

--- src/supermarket/db/migrate/20160815191201_ensure_new_generated_indexes_exist.rb
+++ src/supermarket/db/migrate/20160815191201_ensure_new_generated_indexes_exist.rb
@@ -1,12 +1,6 @@
-class EnsureNewGeneratedIndexesExist < ActiveRecord::Migration[4.2]
+class EnsureNewGeneratedIndexesExist < ActiveRecord::Migration[6.1]
   def up
-    maybe_add_index "accounts", "uid", { name: "index_accounts_on_uid", using: :btree }
-    maybe_add_index "accounts", "username", { name: "index_accounts_on_username", using: :btree }
-  end
-
-  def maybe_add_index(table, columns, opts)
-    unless index_exists?(table, columns, opts)
-      add_index table, columns, opts
-    end
+    add_index "accounts", ["uid"], name: "index_accounts_on_uid", using: :btree, if_not_exists: true
+    add_index "accounts", ["username"], name: "index_accounts_on_username", using: :btree, if_not_exists: true
   end
 end
