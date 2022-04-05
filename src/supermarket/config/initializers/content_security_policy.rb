cabf1531d024c3a933288bf5a3b70edfec688aed
RajeshPaul38 <85236476+RajeshPaul38@users.noreply.github.com>
2022-04-05 17:54:22 +0530

    TEST PR - DO NOT MERGE

--- src/supermarket/config/initializers/content_security_policy.rb
+++ src/supermarket/config/initializers/content_security_policy.rb
@@ -8,7 +8,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, :https
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, :data
-  policy.object_src  :none
+  policy.object_src  :none, :self
   policy.script_src  :self, :https, "https://www.googletagmanager.com", "https://www.google-analytics.com"
   policy.style_src   :self, :https, :unsafe_inline
 
