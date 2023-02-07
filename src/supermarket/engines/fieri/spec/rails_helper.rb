d055af6750a3678c4f30209e218927e2b6230762
Dheeraj Singh Dubey <dhsingh@progress.com>
2023-02-07 08:04:33 +0530

    legacy connection_handling
    Signed-off-by: Dheeraj Singh Dubey <dhsingh@progress.com>

--- src/supermarket/engines/fieri/spec/rails_helper.rb
+++ src/supermarket/engines/fieri/spec/rails_helper.rb
@@ -63,7 +63,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
   config.before(:each) do
     Sidekiq::Worker.clear_all
   end
