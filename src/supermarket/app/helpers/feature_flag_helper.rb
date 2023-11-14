2b8af230b517003b3e2eacbdc40e0371468730fe
ayush-billore <ayush.billore@progress.com>
2023-11-14 10:55:06 +0530

    add feature flag changes
    Signed-off-by: ayush-billore <ayush.billore@progress.com>

--- src/supermarket/app/helpers/feature_flag_helper.rb
+++ src/supermarket/app/helpers/feature_flag_helper.rb
@@ -1,5 +1,13 @@
 module FeatureFlagHelper
-  def air_gapped?
-    ENV["AIR_GAPPED"] == "true"
+  def gtag_enabled?
+    ENV["ENABLE_GTAG"] == "true"
+  end
+
+  def gtm_enabled?
+    ENV["ENABLE_GTM"] == "true"
+  end
+
+  def onetrust_enabled?
+    ENV["ENABLE_ONETRUST"] == "true"
   end
 end
