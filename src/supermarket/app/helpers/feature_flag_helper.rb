d1a9a3269c6e3cd0b730bf0fc6f70b143c0f89cf
ayush-billore <ayush.billore@progress.com>
2023-11-15 11:59:29 +0530

    add cookie policy, privacy policy, logo and favicon changes
    Signed-off-by: ayush-billore <ayush.billore@progress.com>

--- src/supermarket/app/helpers/feature_flag_helper.rb
+++ src/supermarket/app/helpers/feature_flag_helper.rb
@@ -1,13 +1,5 @@
 module FeatureFlagHelper
-  def gtag_enabled?
-    ENV["ENABLE_GTAG"] == "true"
-  end
-
-  def gtm_enabled?
-    ENV["ENABLE_GTM"] == "true"
-  end
-
-  def onetrust_enabled?
-    ENV["ENABLE_ONETRUST"] == "true"
+  def air_gapped?
+    ENV["AIR_GAPPED"] == "true"
   end
 end
