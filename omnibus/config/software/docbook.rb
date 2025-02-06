f62f2b939ebb44a1aaffee68ca25a7d9dd665125
RajeshPaul38 <rajesh.paul@progress.com>
2025-02-07 02:25:22 +0530

    added docbook as a dependency of pg
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>

---
+++ omnibus/config/software/docbook.rb
@@ -0,0 +1,15 @@
+name "docbook"
+default_version "4.5"
+
+dependency "libxml2"
+dependency "libxslt"
+
+source url: "https://www.oasis-open.org/docbook/xml/4.5/docbook-xml-4.5.zip",
+       sha256: "4e4e037a2b83c98c6c94818390d4bdd3f6e10f6ec62dd79188594e26190dc7b4"
+
+relative_path "docbook-4.5"
+
+build do
+  mkdir "#{install_dir}/embedded/share/xml/docbook-4.5"
+  command "cp -r . #{install_dir}/embedded/share/xml/docbook-4.5"
+end
