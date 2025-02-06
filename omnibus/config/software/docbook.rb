0d71407af64f694d0d0a256883aa6d76b5a34413
RajeshPaul38 <rajesh.paul@progress.com>
2025-02-07 02:34:13 +0530

    updated source of docbook
    Signed-off-by: RajeshPaul38 <rajesh.paul@progress.com>

--- omnibus/config/software/docbook.rb
+++ omnibus/config/software/docbook.rb
@@ -4,8 +4,8 @@ default_version "4.5"
 dependency "libxml2"
 dependency "libxslt"
 
-source url: "https://www.oasis-open.org/docbook/xml/4.5/docbook-xml-4.5.zip",
-       sha256: "4e4e037a2b83c98c6c94818390d4bdd3f6e10f6ec62dd79188594e26190dc7b4"
+source url: "http://deb.debian.org/debian/pool/main/d/docbook/docbook_4.5.orig.tar.gz",
+       sha256: "7fed45602dc8734cc95829eec6d965750e5fdf2d3f1c1627659003939e20f419"
 
 relative_path "docbook-4.5"
 
