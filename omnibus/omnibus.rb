29c0fe14834ec03fe10209c49dc5314cbfb235e1
poorndm <poorndm@progress.com>
2025-09-22 21:23:35 +0530

    Set s3-cache- false
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -30,13 +30,13 @@
 
 # Enable S3 asset caching
 # ------------------------------
-  use_s3_caching fasle 
-  s3_access_key  ENV['AWS_ACCESS_KEY_ID']
-  s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
-  s3_bucket      "opscode-omnibus-cache-private"
-  s3_bucket      ENV['AWS_S3_BUCKET']
-  s3_acl         "private"
-  s3_region      "us-west-2"
+use_s3_caching false 
+s3_access_key  ENV['AWS_ACCESS_KEY_ID']
+s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
+s3_bucket      "opscode-omnibus-cache-private"
+s3_bucket      ENV['AWS_S3_BUCKET']
+s3_acl         "private"
+s3_region      "us-west-2"
 
 # Customize compiler bits
 # ------------------------------
