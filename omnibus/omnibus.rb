5f10b8d4d1caa08c80efb2202d1ff16b620371d8
poorndm <poorndm@progress.com>
2025-09-23 10:34:33 +0530

    update s3-env
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -33,8 +33,8 @@
 use_s3_caching false 
 s3_access_key  ENV['AWS_ACCESS_KEY_ID']
 s3_secret_key  ENV['AWS_SECRET_ACCESS_KEY']
-s3_bucket      "opscode-omnibus-cache-private"
-# s3_bucket      ENV['AWS_S3_BUCKET']
+# s3_bucket      "opscode-omnibus-cache-private"
+s3_bucket      ENV['OMNIBUS_S3_BUCKET']
 s3_acl         "private"
 s3_region      "us-west-2"
 
